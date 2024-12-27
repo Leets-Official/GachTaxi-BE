54ad6cd6214b07a88489f12c321f1925659dde26
koreaioi <koreaioi@gachon.ac.kr>
2024-12-28 04:51:03 +0900

    feat: 초기 세팅 - 의존성

---
+++ src/main/java/com/gachtaxi/GachtaxiApplication.java
@@ -0,0 +1,13 @@
+package com.gachtaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachtaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachtaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gachtaxi/GachtaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachtaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachtaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
