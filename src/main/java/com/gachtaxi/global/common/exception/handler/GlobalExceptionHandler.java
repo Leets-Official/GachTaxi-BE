ebe2699af7cd32e7329d9e25f8a7b6910951edad
koreaioi <koreaioi@gachon.ac.kr>
2024-12-30 23:58:25 +0900

    feat: BindException 예외 필드 List 반환

--- src/main/java/com/gachtaxi/global/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/gachtaxi/global/common/exception/handler/GlobalExceptionHandler.java
@@ -1,13 +1,16 @@
 package com.gachtaxi.global.common.exception.handler;
 
 import com.gachtaxi.global.common.exception.BaseException;
+import com.gachtaxi.global.common.exception.ValidErrorResponse;
 import com.gachtaxi.global.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,22 +23,33 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ApiResponse<Void>> handleException(BaseException e) {
-
         return exceptionResponse(e, e.getErrorCode());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handleException(MethodArgumentNotValidException e) {
+    // BindException 처리
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ApiResponse<List<ValidErrorResponse>>> handleValidationException(MethodArgumentNotValidException e) {
+        List<ValidErrorResponse> validErrorResponses = e.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> ValidErrorResponse.builder()
+                        .errorField(fieldError.getField())
+                        .errorMessage(fieldError.getDefaultMessage())
+                        .inputValue(fieldError.getRejectedValue())
+                        .build()
+                ).toList();
 
-        return exceptionResponse(e, BAD_REQUEST);
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), BAD_REQUEST, validErrorResponses);
+        ApiResponse<List<ValidErrorResponse>> response = ApiResponse.response(BAD_REQUEST, "Validation failed", validErrorResponses);
+
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(response);
     }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
-
         return exceptionResponse(e, SERVER_ERROR);
     }
-
 
     // 실제 예외 처리 (log + 응답)
     private ResponseEntity<ApiResponse<Void>> exceptionResponse(Exception e, int errorCode) {
