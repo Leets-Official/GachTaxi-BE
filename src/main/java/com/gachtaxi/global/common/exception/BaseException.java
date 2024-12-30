66da0dad4c7509c0864325b60701352205fcfa4a
koreaioi <koreaioi@gachon.ac.kr>
2024-12-31 02:33:54 +0900

    fix: HttpStatus 적용

--- src/main/java/com/gachtaxi/global/common/exception/BaseException.java
+++ src/main/java/com/gachtaxi/global/common/exception/BaseException.java
@@ -1,15 +1,16 @@
 package com.gachtaxi.global.common.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class BaseException extends RuntimeException {
 
-    private final int errorCode;
+    private final HttpStatus status;
 
-    public BaseException(final String message, final int errorCode) {
+    public BaseException(final String message, final HttpStatus status) {
         super(message);
-        this.errorCode = errorCode;
+        this.status = status;
     }
 }
 
--- src/main/java/com/gachtaxi/global/common/response/ApiResponse.java
+++ src/main/java/com/gachtaxi/global/common/response/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.gachtaxi.global.common.response;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class ApiResponse<T> {
@@ -9,12 +10,12 @@ public class ApiResponse<T> {
     private final String message;
     private final T data;
 
-    public static <T> ApiResponse<T> response(int code, String message, T data){
-        return new ApiResponse<>(code, message, data);
+    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String message, T data){
+        return new ApiResponse<>(httpStatus.value(), message, data);
     }
 
-    public static <T> ApiResponse<T> response(int code, String message){
-        return new ApiResponse<>(code, message, null);
+    public static <T> ApiResponse<T> response(HttpStatus httpStatus, String message){
+        return new ApiResponse<>(httpStatus.value(), message, null);
     }
 
     public ApiResponse(int code, String message, T data) {
