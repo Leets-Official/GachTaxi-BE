59e73209fe1391d36745b0be2170054fc73b2574
koreaioi <koreaioi@gachon.ac.kr>
2024-12-31 02:37:45 +0900

    remove: remove @Getter

--- src/main/java/com/gachtaxi/global/common/exception/BaseException.java
+++ src/main/java/com/gachtaxi/global/common/exception/BaseException.java
@@ -1,9 +1,7 @@
 package com.gachtaxi.global.common.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 public abstract class BaseException extends RuntimeException {
 
     private final HttpStatus status;
