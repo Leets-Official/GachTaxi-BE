a7e437bdfddd0ab01aacf5582e19717d1e0d9013
koreaioi <koreaioi@gachon.ac.kr>
2024-12-31 02:39:26 +0900

    feat: add @Getter to BaseException

--- src/main/java/com/gachtaxi/global/common/exception/BaseException.java
+++ src/main/java/com/gachtaxi/global/common/exception/BaseException.java
@@ -1,7 +1,9 @@
 package com.gachtaxi.global.common.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public abstract class BaseException extends RuntimeException {
 
     private final HttpStatus status;
