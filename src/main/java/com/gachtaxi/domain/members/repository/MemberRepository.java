f929606574c4399a9990954f6c1d9f2482cea1f4
koreaioi <koreaioi@gachon.ac.kr>
2025-01-02 21:06:11 +0900

    feat: MemberRepository existsByEmail 추가

--- src/main/java/com/gachtaxi/domain/members/repository/MemberRepository.java
+++ src/main/java/com/gachtaxi/domain/members/repository/MemberRepository.java
@@ -4,7 +4,10 @@ import com.gachtaxi.domain.members.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Members, Long> {
 
+    Boolean existsByEmail(String email);
 }
