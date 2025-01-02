b372ad8bf0a74c577a0d86098f2ff2bf77b4bc5d
koreaioi <koreaioi@gachon.ac.kr>
2025-01-02 18:23:32 +0900

    feat: MemberService 추가

---
+++ src/main/java/com/gachtaxi/domain/members/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.gachtaxi.domain.members.service;
+
+import com.gachtaxi.domain.members.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+}
