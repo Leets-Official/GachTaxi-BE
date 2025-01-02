e5b167c32fd481efa210c1918c74f9dcca8bba9a
koreaioi <koreaioi@gachon.ac.kr>
2025-01-02 21:01:42 +0900

    feat: KakaoUtil - requestKakaoToken 추가

--- src/main/java/com/gachtaxi/global/auth/kakao/util/KakaoUtil.java
+++ src/main/java/com/gachtaxi/global/auth/kakao/util/KakaoUtil.java
@@ -2,8 +2,13 @@ package com.gachtaxi.global.auth.kakao.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
 
+import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
 
 /*
 * KakaoUtil은 외부 API 통신 및 카카오 관련 작업을 담당(책임)한다.
@@ -24,4 +29,21 @@ public class KakaoUtil {
 
     @Value("${gachtaxi.auth.kakao.user_profile}")
     private String kakaoUserProfileUri;
+
+    private final RestClient restClient = RestClient.create();
+
+    public KakaoAccessToken reqeustKakaoToken(String authCode){
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", kakaoClient);
+        params.add("redirect_url", kakaoRedirect);
+        params.add("code", authCode);
+
+        return restClient.post()
+                .uri(kakaoTokenUri)
+                .body(params)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .retrieve()
+                .body(KakaoAccessToken.class);
+    }
 }
