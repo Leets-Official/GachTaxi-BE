ef75afe2593f0e0282b9d045da0528a253baefe5
koreaioi <koreaioi@gachon.ac.kr>
2025-01-03 04:06:17 +0900

    feat: CookieUtil 추가

---
+++ src/main/java/com/gachtaxi/global/auth/jwt/util/CookieUtil.java
@@ -0,0 +1,37 @@
+package com.gachtaxi.global.auth.jwt.util;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    @Value("${gachtaxi.auth.jwt.cookieMaxAge}")
+    private Long cookieMaxAge;
+
+    public void setCookie( String name, String value, HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from(name, value)
+                .maxAge(cookieMaxAge)
+                .path("/")
+                .secure(false) //https 적용 시 true
+                .httpOnly(true)
+                .sameSite("None")
+                .build();
+
+        response.setHeader("Set-Cookie", cookie.toString());
+    }
+
+    public void deleteCookie(HttpServletResponse response, String name) {
+        ResponseCookie cookie = ResponseCookie.from(name, "value")
+                .maxAge(0)
+                .path("/")
+                .secure(false)
+                .httpOnly(true)
+                .sameSite("None")
+                .build();
+
+        response.setHeader("Set-Cookie", cookie.toString());
+    }
+}
