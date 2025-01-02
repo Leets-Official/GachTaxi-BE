99bf853f172d647cb1f3ce3c022abf440d889e95
hyxklee <ewgt1234@naver.com>
2025-01-02 10:30:11 +0900

    feat: 채팅을 위한 레디스 전역 리스너 설정

--- src/main/java/com/gachtaxi/global/config/RedisConfig.java
+++ src/main/java/com/gachtaxi/global/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.gachtaxi.global.config;
 
 import com.gachtaxi.domain.chat.dto.request.ChatMessage;
+import com.gachtaxi.domain.chat.redis.RedisChatSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,15 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    public static final String WILD_CARD = "*";
 
     @Value("${spring.data.redis.host}")
     private String host;
@@ -23,6 +27,9 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.password}")
     private String password;
+
+    @Value("${chat.topic}")
+    public String chatTopic;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -36,13 +43,23 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, ChatMessage> chatRedisTemplate() {
+    public RedisTemplate<String, ChatMessage> chatRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, ChatMessage> redisTemplate = new RedisTemplate<>();
 
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
 
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory,
+                                                                       RedisChatSubscriber redisChatSubscriber) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(redisChatSubscriber, new PatternTopic(chatTopic + WILD_CARD));
+
+        return container;
     }
 }
