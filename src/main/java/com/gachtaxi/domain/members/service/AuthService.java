42ada4604a714e59563965684f99554472fc94f9
koreaioi <koreaioi@gachon.ac.kr>
2025-01-02 21:53:16 +0900

    feat: AuthService - 회원가입 여부로 분기 처리(jwt 발급 전)

--- src/main/java/com/gachtaxi/domain/members/service/AuthService.java
+++ src/main/java/com/gachtaxi/domain/members/service/AuthService.java
@@ -1,12 +1,15 @@
 package com.gachtaxi.domain.members.service;
 
 import com.gachtaxi.global.auth.kakao.util.KakaoUtil;
+import com.gachtaxi.global.auth.mapper.OauthMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
+import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
+import static com.gachtaxi.global.auth.dto.OauthLoginResponse.*;
+
 
 /*
 * AuthService는 인증 로직 책임을 가진다.
@@ -18,23 +21,26 @@ import com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
 public class AuthService {
 
     private final KakaoUtil kakaoUtil;
+    private final OauthMapper oauthMapper;
     private final MemberService memberService;
 
-    public String kakaoLogin(String authCode, HttpServletResponse response) {
+    public kakaoLoginResponse kakaoLogin(String authCode, HttpServletResponse response) {
         // 인가 코드로 토큰 발급
         KakaoAccessToken kakaoAccessToken = kakaoUtil.reqeustKakaoToken(authCode);
 
         // 토큰으로 사용자 정보(email) 가져오기
-        KakaoUserInfoResponse kakaoUserInfoResponse = kakaoUtil.requestKakaoProfile(kakaoAccessToken.access_token());
+        KakaoUserInfoResponse userInfo = kakaoUtil.requestKakaoProfile(kakaoAccessToken.access_token());
 
         // 회원가입 여부로 분기 처리
-        String email = kakaoUserInfoResponse.kakao_account().email();
+        String email = userInfo.kakao_account().email();
         if(memberService.checkByEmail(email)){
             log.info("회원가입 한 사용자");
-        }{
-            log.info("회원가입 하지 않은 사용자");
-        }
+            // jwt 토큰 발급 (추후 추가)
 
-        return kakaoAccessToken.access_token();
+            // 응답
+            return oauthMapper.toKakaoLoginResponse(userInfo);
+        }else{
+            return oauthMapper.toKakaoUnRegisterResponse(userInfo);
+        }
     }
 }
