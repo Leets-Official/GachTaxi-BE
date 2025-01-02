73573a57f0e6a00fc57600e551d6b0d28db542be
koreaioi <koreaioi@gachon.ac.kr>
2025-01-02 21:52:41 +0900

    feat: OauthMapper - 카카오 관련Response 추가

---
+++ src/main/java/com/gachtaxi/global/auth/mapper/OauthMapper.java
@@ -0,0 +1,28 @@
+package com.gachtaxi.global.auth.mapper;
+
+import org.springframework.stereotype.Component;
+
+import static com.gachtaxi.global.auth.enums.OauthLoginStatus.*;
+import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
+import static com.gachtaxi.global.auth.dto.OauthLoginResponse.*;
+
+@Component
+public class OauthMapper {
+
+    public kakaoLoginResponse toKakaoUnRegisterResponse(KakaoUserInfoResponse userInfo) {
+        return kakaoLoginResponse.builder()
+                .kakaoId(userInfo.id())
+                .status(UN_REGISTER)
+                .build();
+    }
+
+    // jwt 토큰 추가 할 것.
+    public kakaoLoginResponse toKakaoLoginResponse(KakaoUserInfoResponse userInfo) {
+        return kakaoLoginResponse.builder()
+                .kakaoId(userInfo.id())
+                .status(LOGIN)
+                .build();
+    }
+
+
+}
