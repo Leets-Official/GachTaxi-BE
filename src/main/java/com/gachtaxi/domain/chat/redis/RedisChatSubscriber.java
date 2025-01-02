4edb6267d610b83380b602fd6ce8ef3150e43080
hyxklee <ewgt1234@naver.com>
2025-01-02 10:32:08 +0900

    feat: 채팅을 위한 RedisSubscriber 구현 및 예외처리

---
+++ src/main/java/com/gachtaxi/domain/chat/redis/RedisChatSubscriber.java
@@ -0,0 +1,37 @@
+package com.gachtaxi.domain.chat.redis;
+
+import com.gachtaxi.domain.chat.Exception.CustomMessagingException;
+import com.gachtaxi.domain.chat.Exception.CustomSerializationException;
+import com.gachtaxi.domain.chat.Exception.RedisSubscriberException;
+import com.gachtaxi.domain.chat.dto.request.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.SerializationException;
+import org.springframework.messaging.MessagingException;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisChatSubscriber implements MessageListener {
+
+    private final RedisTemplate<String, ChatMessage> chatRedisTemplate;
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        try {
+            ChatMessage chatMessage = (ChatMessage) chatRedisTemplate.getValueSerializer().deserialize(message.getBody());
+
+            simpMessageSendingOperations.convertAndSend("/sub/chat/room/" + chatMessage.roomId(), chatMessage);
+        } catch (MessagingException e) {
+            throw new CustomMessagingException(e.getMessage());
+        } catch (SerializationException e) {
+            throw new CustomSerializationException(e.getMessage());
+        } catch (Exception e) {
+            throw new RedisSubscriberException();
+        }
+    }
+}
