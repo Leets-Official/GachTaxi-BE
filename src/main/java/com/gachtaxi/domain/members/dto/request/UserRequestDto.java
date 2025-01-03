227f8146c1d0bcfe0986e609959401b920b6ebc3
koreaioi <koreaioi@gachon.ac.kr>
2025-01-04 03:46:29 +0900

    fix: SignUpDto 유효성 검증 어노테이션 추가

--- src/main/java/com/gachtaxi/domain/members/dto/request/UserRequestDto.java
+++ src/main/java/com/gachtaxi/domain/members/dto/request/UserRequestDto.java
@@ -3,21 +3,20 @@ package com.gachtaxi.domain.members.dto.request;
 
 import com.gachtaxi.domain.members.entity.enums.Gender;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class UserRequestDto {
 
     public record SignUpDto(
-            //String profilePicture,
             @NotBlank String email,
             @NotBlank String nickName,
             @NotBlank String realName,
             @NotBlank String studentNumber,
-            //String phoneNumber,
             @NotBlank Gender gender,
-            Boolean termsAgreement,
-            Boolean privacyAgreement,
-            Boolean marketingAgreement,
-            Boolean twoFactorAuthentication,
+            @NotNull Boolean termsAgreement,
+            @NotNull Boolean privacyAgreement,
+            @NotNull Boolean marketingAgreement,
+            @NotNull Boolean twoFactorAuthentication,
             Long kakaoId,
             Long googleId
     ){}
