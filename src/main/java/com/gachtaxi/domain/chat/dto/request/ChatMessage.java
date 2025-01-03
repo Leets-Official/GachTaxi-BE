d1722eb373c0cd394a4190cbff404eec3393588b
hyxklee <ewgt1234@naver.com>
2025-01-03 13:44:22 +0900

    feat: 닉네임 추가 및 정적 팩토리 메서드 추가 구현

--- src/main/java/com/gachtaxi/domain/chat/dto/request/ChatMessage.java
+++ src/main/java/com/gachtaxi/domain/chat/dto/request/ChatMessage.java
@@ -1,14 +1,24 @@
 package com.gachtaxi.domain.chat.dto.request;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record ChatMessage(
-        long roomId,
-        long senderId,
+        Long roomId,
+        Long senderId,
+        String nickName,
         String message,
         LocalDateTime timeStamp
 ) {
-    public static ChatMessage of(long roomId, long senderId, String message, LocalDateTime timeStamp) {
-        return new ChatMessage(roomId, senderId, message, timeStamp);
+    public static ChatMessage of(ChatMessageRequest request, long senderId, LocalDateTime timeStamp) {
+        return ChatMessage.builder()
+                .roomId(request.roomId())
+                .senderId(senderId)
+                .nickName(request.nickName())
+                .message(request.message())
+                .timeStamp(timeStamp)
+                .build();
     }
 }
