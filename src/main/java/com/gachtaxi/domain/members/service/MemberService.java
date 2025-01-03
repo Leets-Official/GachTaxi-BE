c951f78c3c80f900350468e1facefeb77da1280b
koreaioi <koreaioi@gachon.ac.kr>
2025-01-03 21:14:58 +0900

    feat: MemberService - saveMember() 추가

--- src/main/java/com/gachtaxi/domain/members/service/MemberService.java
+++ src/main/java/com/gachtaxi/domain/members/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.gachtaxi.domain.members.service;
 
+import com.gachtaxi.domain.members.dto.request.UserRequestDto;
 import com.gachtaxi.domain.members.entity.Members;
+import com.gachtaxi.domain.members.exception.DuplicatedStudentNumberException;
 import com.gachtaxi.domain.members.repository.MemberRepository;
+import com.gachtaxi.global.auth.jwt.service.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +15,28 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final JwtService jwtService;
     private final MemberRepository memberRepository;
 
-    public Boolean checkByEmail(String email) {
-        return memberRepository.existsByEmail(email);
+    public void saveMember(UserRequestDto.registerDto dto, HttpServletResponse response) {
+        validStudentNumber(dto);
+        Members newMember = Members.of(dto);
+        jwtService.responseJwtToken(newMember.getId(), newMember.getEmail(), newMember.getRole(), response);
+        memberRepository.save(newMember);
     }
 
-    public Optional<Members> findByEmail(String email) {
-        return memberRepository.findByEmail(email);
+    public Optional<Members> findByKakaoId(Long kakaoId) {
+        return memberRepository.findByKakaoId(kakaoId);
     }
 
+    /*
+    * refactor
+    * */
+
+    private void validStudentNumber(UserRequestDto.registerDto dto) {
+        String studentNumber = dto.studentNumber();
+        memberRepository.findByStudentNumber(studentNumber).ifPresent(m -> {
+            throw new DuplicatedStudentNumberException();
+        });
+    }
 }
