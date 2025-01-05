e17dc0a24baf20c1dd24e1af0a86a342ce93f73d
hyxklee <ewgt1234@naver.com>
2025-01-05 22:14:01 +0900

    feat: stomp 커맨드별 전략 구현

---
+++ src/main/java/com/gachtaxi/domain/chat/interceptor/strategy/DefaultCommandStrategy.java
@@ -0,0 +1,23 @@
+package com.gachtaxi.domain.chat.interceptor.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DefaultCommandStrategy implements StompCommandStrategy{
+
+    @Override
+    public boolean supports(StompCommand command) {
+        return false;
+    }
+
+    @Override
+    public Message<?> preSend(Message<?> message, StompHeaderAccessor accessor, MessageChannel channel) {
+        return message;
+    }
+}
