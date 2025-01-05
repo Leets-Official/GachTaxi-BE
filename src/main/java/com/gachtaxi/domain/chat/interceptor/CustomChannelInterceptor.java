7a80806931f3f75498fc3b135a651663ffc5f72e
hyxklee <ewgt1234@naver.com>
2025-01-05 22:25:15 +0900

    refactor: 개행 추가 및 로거 제거

--- src/main/java/com/gachtaxi/domain/chat/interceptor/CustomChannelInterceptor.java
+++ src/main/java/com/gachtaxi/domain/chat/interceptor/CustomChannelInterceptor.java
@@ -2,18 +2,17 @@ package com.gachtaxi.domain.chat.interceptor;
 
 import com.gachtaxi.domain.chat.interceptor.strategy.ChatStrategyHandler;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomChannelInterceptor implements ChannelInterceptor {
     private final ChatStrategyHandler chatStrategyHandler;
+
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
