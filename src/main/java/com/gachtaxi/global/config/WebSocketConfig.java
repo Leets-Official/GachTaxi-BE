3c00d6d681a461cd651f7526029f9c62a0098c20
hyxklee <ewgt1234@naver.com>
2025-01-05 22:19:24 +0900

    refactor: 웹소켓 cors 경로 수정

--- src/main/java/com/gachtaxi/global/config/WebSocketConfig.java
+++ src/main/java/com/gachtaxi/global/config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:3001");
+                .setAllowedOriginPatterns("http://localhost:3000");
     }
 
     @Override
