8f631dbe27f9e2a49aaa3786bee4308f42b2b909
koreaioi <koreaioi@gachon.ac.kr>
2025-01-06 21:02:11 +0900

    feat: 토큰 재발급 성공 메세지

--- src/main/java/com/gachtaxi/domain/members/controller/ResponseMessage.java
+++ src/main/java/com/gachtaxi/domain/members/controller/ResponseMessage.java
@@ -10,6 +10,7 @@ public enum ResponseMessage {
     REGISTER_SUCCESS("회원가입에 성공했습니다."),
 
     // AuthController
+    REFRESH_TOKEN_REISSUE("토큰 재발급에 성공했습니다."),
     LOGIN_SUCCESS("로그인 성공에 성공했습니다."),
     UN_REGISTER("회원가입을 진행해주세요");
 
