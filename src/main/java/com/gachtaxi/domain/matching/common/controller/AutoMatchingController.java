df9d08079d3c6d4aaa41a88749ce57bec079714a
senna <kjy915875@gmail.com>
2025-01-06 23:18:25 +0900

    feat: 자동 매칭 컨트롤러 구현

---
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/AutoMatchingController.java
@@ -0,0 +1,56 @@
+package com.gachtaxi.domain.matching.common.controller;
+
+import com.gachtaxi.domain.matching.common.dto.request.AutoMatchingPostRequest;
+import com.gachtaxi.domain.matching.common.dto.response.AutoMatchingPostResponse;
+import com.gachtaxi.domain.matching.common.service.MatchingService;
+import com.gachtaxi.global.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/matching/auto")
+public class AutoMatchingController {
+
+  private final MatchingService matchingService;
+
+  @GetMapping("/subscribe")
+  public ApiResponse<SseEmitter> subscribeSse(@RequestParam Long memberId) {
+    // TODO: 인가 로직 완성되면 해당 멤버의 아이디를 가져오도록 변경
+//    Long memberId = 1L;
+    return ApiResponse.response(
+        HttpStatus.OK,
+        ResponseMessage.SUBSCRIBE_SUCCESS.getMessage(),
+        this.matchingService.handleSubscribe(memberId)
+    );
+  }
+
+  @PostMapping("/request")
+  public ApiResponse<AutoMatchingPostResponse> requestMatching(
+      @RequestParam Long memberId,
+      @RequestBody AutoMatchingPostRequest autoMatchingPostRequest
+  ) {
+    // TODO: 인가 로직 완성되면 해당 멤버의 아이디를 가져오도록 변경
+//    Long memberId = 1L;
+    if (!this.matchingService.isSseSubscribed(memberId)) {
+      return ApiResponse.response(
+          HttpStatus.BAD_REQUEST,
+          ResponseMessage.NOT_SUBSCRIBED_SSE.getMessage()
+      );
+    }
+
+    return ApiResponse.response(
+        HttpStatus.OK,
+        ResponseMessage.AUTO_MATCHING_REQUEST_ACCEPTED.getMessage(),
+        this.matchingService.handlerAutoRequestMatching(memberId, autoMatchingPostRequest)
+    );
+  }
+}
