43fa66b888e65b0ae4698bbec74879b8db17039b
koreaioi <koreaioi@gachon.ac.kr>
2025-01-06 21:09:37 +0900

    fix: JwtService - 토큰 재발급 로직 추가

--- src/main/java/com/gachtaxi/domain/members/controller/AuthController.java
+++ src/main/java/com/gachtaxi/domain/members/controller/AuthController.java
@@ -2,14 +2,17 @@ package com.gachtaxi.domain.members.controller;
 
 import com.gachtaxi.domain.members.service.AuthService;
 import com.gachtaxi.global.auth.enums.OauthLoginStatus;
+import com.gachtaxi.global.auth.jwt.dto.JwtTokenDto;
+import com.gachtaxi.global.auth.jwt.service.JwtService;
 import com.gachtaxi.global.common.response.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import static com.gachtaxi.domain.members.controller.ResponseMessage.*;
-import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
+import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.OauthKakaoResponse;
 
 @RequestMapping("/auth")
 @RestController
@@ -17,6 +20,7 @@ import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtService jwtService;
 
     @GetMapping("/login/kakao")
     public ApiResponse<OauthKakaoResponse> kakaoLogin(@RequestParam("code") String authcode, HttpServletResponse response) {
@@ -25,5 +29,14 @@ public class AuthController {
                 ? LOGIN_SUCCESS
                 : UN_REGISTER;
         return ApiResponse.response(HttpStatus.OK, OAUTH_STATUS.getMessage(), res);
+    }
+
+    @PostMapping("/refresh")
+    public ApiResponse<Void> reissueRefreshToken(HttpServletRequest request, HttpServletResponse response) {
+        JwtTokenDto jwtTokenDto = jwtService.reissueJwtToken(request);
+
+        jwtService.setCookie(jwtTokenDto.refreshToken(), response);
+        jwtService.setHeader(jwtTokenDto.accessToken(), response);
+        return ApiResponse.response(HttpStatus.OK, REFRESH_TOKEN_REISSUE.getMessage());
     }
 }
