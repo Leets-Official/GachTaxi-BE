a21fbe1d2fa23e06beb7a7a9c2b6a309aa43bffe
senna <kjy915875@gmail.com>
2025-01-07 00:51:11 +0900

    refactor: 방 생성 이벤트 DTO에 필요한 필드들 추가

--- src/main/java/com/gachtaxi/domain/matching/event/dto/kafka_topic/MatchRoomCreatedEvent.java
+++ src/main/java/com/gachtaxi/domain/matching/event/dto/kafka_topic/MatchRoomCreatedEvent.java
@@ -1,19 +1,26 @@
 package com.gachtaxi.domain.matching.event.dto.kafka_topic;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.gachtaxi.domain.matching.common.entity.enums.Tags;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record MatchRoomCreatedEvent(
-    Long roomId,
     Long hostMemberId,
+    Integer maxCapacity,
+    String title,
+    String description,
+    Integer totalCharge,
+    String startPoint,
+    String startName,
+    String destinationPoint,
+    String destinationName,
+    List<Tags> criteria,
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime createdAt
 ) {
 
-  public static MatchRoomCreatedEvent of(Long roomId, Long memberId) {
-    return new MatchRoomCreatedEvent(roomId, memberId, LocalDateTime.now());
-  }
 }
