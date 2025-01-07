1961fb6c0f4b88fc6131af2c2ddfe70b4f3ebb88
senna <kjy915875@gmail.com>
2025-01-07 14:15:07 +0900

    docs: 매칭 알고리즘에서 추가해야할 로직 명시

--- src/main/java/com/gachtaxi/domain/matching/algorithm/service/MatchingAlgorithmService.java
+++ src/main/java/com/gachtaxi/domain/matching/algorithm/service/MatchingAlgorithmService.java
@@ -9,6 +9,7 @@ public interface MatchingAlgorithmService {
 
   /**
    * 방을 찾는 메서드
+   * 이미 방에 들어가있는 멤버가 다시 요청했을 때 Optional.empty()를 반환하도록 로직을 구성해야함
    * @param userId 방에 들어가려는 사용자 ID
    * @param criteria 방 검색에 필요한 기타 조건 (ex. 위치, 키워드 등)
    * @return Optional<FindRoomResult> - 매칭 가능한 방 정보가 있으면 값이 있고, 없으면 empty
