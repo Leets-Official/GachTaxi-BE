fb59c772b6c6a3603417cee31cb138723f382a66
koreaioi <koreaioi@gachon.ac.kr>
2025-01-07 21:23:13 +0900

    feat: Merge

--- src/main/java/com/gachtaxi/global/auth/jwt/exception/JwtErrorMessage.java
+++ src/main/java/com/gachtaxi/global/auth/jwt/exception/JwtErrorMessage.java
@@ -13,7 +13,9 @@ public enum JwtErrorMessage {
     USER_NOT_FOUND_EMAIL("해당 이메일의 유저를 찾을 수 없습니다"),
     JWT_TOKEN_NOT_FOUND("토큰을 찾을 수 없습니다"),
     JWT_TOKEN_EXPIRED("만료된 토큰입니다."),
-    JWT_TOKEN_INVALID("유효하지 않은 토큰 입니다.");
+    JWT_TOKEN_INVALID("유효하지 않은 토큰 입니다."),
+    JWT_TOKEN_UN_VALID("유효하지 않은 토큰 입니다."),
+    JWT_TOKEN_NOT_EXIST("헤더에 인증 토큰이 존재하지 않습니다");
 
     private final String message;
 }
