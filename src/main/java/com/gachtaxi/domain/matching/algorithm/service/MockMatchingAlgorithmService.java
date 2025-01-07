b6375d7c8b6b3e9f81bc650f6b2052603e81e79a
senna <kjy915875@gmail.com>
2025-01-07 14:15:22 +0900

    refactor: 테스트를 위해 mock 매칭 알고리즘 로직 수정

--- src/main/java/com/gachtaxi/domain/matching/algorithm/service/MockMatchingAlgorithmService.java
+++ src/main/java/com/gachtaxi/domain/matching/algorithm/service/MockMatchingAlgorithmService.java
@@ -1,7 +1,9 @@
 package com.gachtaxi.domain.matching.algorithm.service;
 
 import com.gachtaxi.domain.matching.algorithm.dto.FindRoomResult;
+import com.gachtaxi.domain.matching.common.entity.MatchingRoom;
 import com.gachtaxi.domain.matching.common.entity.enums.Tags;
+import com.gachtaxi.domain.matching.common.repository.MatchingRoomRepository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -11,13 +13,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MockMatchingAlgorithmService implements MatchingAlgorithmService {
 
+  private final MatchingRoomRepository matchingRoomRepository;
 
   @Override
   public Optional<FindRoomResult> findRoom(Long userId, List<Tags> criteria) {
-    if (userId == 1L) {
-      return Optional.of(FindRoomResult.builder()
-          .roomId(1L)
-          .build());
+    List<MatchingRoom> matchingRoomList = this.matchingRoomRepository.findAll();
+    if (!matchingRoomList.isEmpty()) {
+      MatchingRoom matchingRoom = matchingRoomList.get(0);
+      return Optional.of(
+          FindRoomResult.builder()
+              .roomId(matchingRoom.getId())
+              .maxCapacity(matchingRoom.getCapacity())
+              .build());
     }
     return Optional.empty();
   }
