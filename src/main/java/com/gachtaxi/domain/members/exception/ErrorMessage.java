4b849cf5912ce4a417b2a8ebbade028c9fee0d86
koreaioi <koreaioi@gachon.ac.kr>
2025-01-08 23:20:07 +0900

    feat: 이메일 형식 예외 메세지 추가

--- src/main/java/com/gachtaxi/domain/members/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/members/exception/ErrorMessage.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorMessage {
 
     DUPLICATED_STUDENT_NUMBER("이미 가입된 학번입니다."),
-    MEMBER_NOT_FOUND("회원을 찾을 수 없습니다.");
+    MEMBER_NOT_FOUND("회원을 찾을 수 없습니다."),
+    EMAIL_FROM_INVALID("가천대 이메일 형식이 아닙니다.");
 
     private final String message;
 }
