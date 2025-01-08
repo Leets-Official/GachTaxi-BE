831c7f3a69126494f508e37833251044eb8f7012
koreaioi <koreaioi@gachon.ac.kr>
2025-01-08 22:28:33 +0900

    feat: 인증 코드 이메일 전송 로직 구현

---
+++ src/main/java/com/gachtaxi/domain/members/controller/InactiveMemberController.java
@@ -0,0 +1,32 @@
+package com.gachtaxi.domain.members.controller;
+
+import com.gachtaxi.domain.members.dto.response.InactiveMemberResponseDto;
+import com.gachtaxi.global.auth.jwt.annotation.CurrentMemberId;
+import com.gachtaxi.global.common.mail.dto.request.EmailAddressDto;
+import com.gachtaxi.global.common.mail.service.EmailService;
+import com.gachtaxi.global.common.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.gachtaxi.domain.members.controller.ResponseMessage.EMAIL_SEND_SUCCESS;
+import static org.springframework.http.HttpStatus.OK;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class InactiveMemberController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/tmp-members/mail-authcode")
+    public ApiResponse sendEmail(
+            @RequestBody EmailAddressDto emailDto,
+            @CurrentMemberId Long userId
+    ) {
+        emailService.sendEmail(emailDto.email());
+        return ApiResponse.response(OK, EMAIL_SEND_SUCCESS.getMessage(), InactiveMemberResponseDto.from(userId));
+    }
+}
--- src/main/java/com/gachtaxi/domain/members/controller/ResponseMessage.java
+++ src/main/java/com/gachtaxi/domain/members/controller/ResponseMessage.java
@@ -12,7 +12,10 @@ public enum ResponseMessage {
     // AuthController
     REFRESH_TOKEN_REISSUE("토큰 재발급에 성공했습니다."),
     LOGIN_SUCCESS("로그인 성공에 성공했습니다."),
-    UN_REGISTER("회원가입을 진행해주세요");
+    UN_REGISTER("회원가입을 진행해주세요"),
+
+    //InactiveMemberController
+    EMAIL_SEND_SUCCESS("이메일을 성공적으로 전송했습니다");
 
     private final String message;
 }
