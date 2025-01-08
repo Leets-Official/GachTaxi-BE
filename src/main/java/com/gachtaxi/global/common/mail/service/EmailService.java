585687bc4f563606b5ac8321f3f268c3668f9b6f
koreaioi <koreaioi@gachon.ac.kr>
2025-01-08 22:24:41 +0900

    feat: EmailService 인증코드 이메일 전송

---
+++ src/main/java/com/gachtaxi/global/common/mail/service/EmailService.java
@@ -0,0 +1,63 @@
+package com.gachtaxi.global.common.mail.service;
+
+import com.gachtaxi.global.common.redis.RedisUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.Destination;
+import software.amazon.awssdk.services.ses.model.SendTemplatedEmailRequest;
+
+import java.security.SecureRandom;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private static final String CODE_LENGTH = "%06d";
+    private static final String CODE_FORMAT = "{\"code\":\"%s\"}";
+    private static final int BOUND = 888888;
+    private static final int OFFSET = 111111;
+
+    private final SesClient sesClient;
+    private final RedisUtil redisUtil;
+    private final SecureRandom secureRandom = new SecureRandom();
+
+    @Value("${aws.ses.templateName}")
+    private String emailTemplateName;
+    @Value("${aws.ses.from}")
+    private String senderEmail;
+
+    public void sendEmail(String recipientEmail) {
+        String code = generateCode();
+        redisUtil.setEmailAuthCode(recipientEmail, code);
+
+        sendAuthCodeEmail(recipientEmail, code);
+    }
+
+    // 인증 코드 검증
+
+    /*
+    * refactoring
+    * */
+
+    private String generateCode() {
+        return String.format(CODE_LENGTH, secureRandom.nextInt(BOUND) + OFFSET);
+    }
+
+    // @Async 비동기 처리??
+    private void sendAuthCodeEmail(String recipientEmail, String code) {
+        String templateData = String.format(CODE_FORMAT, code);
+
+        SendTemplatedEmailRequest request = SendTemplatedEmailRequest.builder()
+                .destination(Destination.builder().toAddresses(recipientEmail).build())
+                .template(emailTemplateName)
+                .templateData(templateData)
+                .source(senderEmail)
+                .build();
+
+        sesClient.sendTemplatedEmail(request);
+    }
+
+
+}
