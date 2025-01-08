44f03d89f7fdfcaefb06f6d3558682e3a2446c29
koreaioi <koreaioi@gachon.ac.kr>
2025-01-08 23:06:53 +0900

    feat: 이메일 인증 및 이메일 정보 추가 API 구현

--- src/main/java/com/gachtaxi/domain/members/controller/InactiveMemberController.java
+++ src/main/java/com/gachtaxi/domain/members/controller/InactiveMemberController.java
@@ -1,17 +1,17 @@
 package com.gachtaxi.domain.members.controller;
 
+import com.gachtaxi.domain.members.dto.request.InactiveMemberAuthCodeRequestDto;
 import com.gachtaxi.domain.members.dto.response.InactiveMemberResponseDto;
+import com.gachtaxi.domain.members.service.MemberService;
 import com.gachtaxi.global.auth.jwt.annotation.CurrentMemberId;
 import com.gachtaxi.global.common.mail.dto.request.EmailAddressDto;
 import com.gachtaxi.global.common.mail.service.EmailService;
 import com.gachtaxi.global.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import static com.gachtaxi.domain.members.controller.ResponseMessage.EMAIL_AUTHENTICATION_SUCESS;
 import static com.gachtaxi.domain.members.controller.ResponseMessage.EMAIL_SEND_SUCCESS;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -21,6 +21,7 @@ import static org.springframework.http.HttpStatus.OK;
 public class InactiveMemberController {
 
     private final EmailService emailService;
+    private final MemberService memberService;
 
     @PostMapping("/tmp-members/mail-authcode")
     public ApiResponse sendEmail(
@@ -29,5 +30,15 @@ public class InactiveMemberController {
     ) {
         emailService.sendEmail(emailDto.email());
         return ApiResponse.response(OK, EMAIL_SEND_SUCCESS.getMessage(), InactiveMemberResponseDto.from(userId));
+    }
+
+    @PatchMapping("/tmp-members/mail-authcode")
+    public ApiResponse checkAuthCodeAndUpdateEmail(
+            @RequestBody @Valid InactiveMemberAuthCodeRequestDto dto,
+            @CurrentMemberId Long userId
+    ) {
+        emailService.checkEmailAuthCode(dto.email(), dto.authCode());
+        memberService.updateInactiveMemberOfEmail(dto.email(), userId);
+        return ApiResponse.response(OK, EMAIL_AUTHENTICATION_SUCESS.getMessage(), InactiveMemberResponseDto.from(userId));
     }
 }
