a3a765162e30fc1456db1167927170fda7113145
hyxklee <ewgt1234@naver.com>
2025-01-09 19:25:46 +0900

    feat: 웹소켓 세션 에러 메시지 추가

--- src/main/java/com/gachtaxi/domain/chat/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/chat/exception/ErrorMessage.java
@@ -13,6 +13,7 @@ public enum ErrorMessage {
     REDIS_SUB_ERROR("[Redis] 메시지 전송에 실패했습니다."),
     CHAT_SUBSCRIBE_ERROR("올바르지 않은 채팅 구독 경로입니다."),
     CHATTING_ROOM_NOT_FOUND("존재하지 않는 채팅방입니다."),
-    CHAT_SEND_END_POINT_ERROR("올바르지 않은 채팅 메시지 경로입니다.");
+    CHAT_SEND_END_POINT_ERROR("올바르지 않은 채팅 메시지 경로입니다."),
+    WEB_SOCKET_SESSION_ATTR_NOT_FOUND(" 가 웹소켓 세션에 존재하지 않습니다.");
     private final String message;
 }
