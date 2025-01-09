5a369a4c4cfabcd1c84a6b2d584c68e51f06bdc0
koreaioi <koreaioi@gachon.ac.kr>
2025-01-09 14:54:31 +0900

    fix: @RequiredArgsConstructor 추가

--- src/main/java/com/gachtaxi/global/common/mail/service/SesClientTemplateService.java
+++ src/main/java/com/gachtaxi/global/common/mail/service/SesClientTemplateService.java
@@ -1,19 +1,17 @@
 package com.gachtaxi.global.common.mail.service;
 
 import com.gachtaxi.global.common.mail.dto.request.NewTemplateRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.ses.SesClient;
 import software.amazon.awssdk.services.ses.model.CreateTemplateRequest;
 import software.amazon.awssdk.services.ses.model.Template;
 
 @Service
+@RequiredArgsConstructor
 public class SesClientTemplateService {
 
     private final SesClient sesClient;
-
-    public SesClientTemplateService(SesClient sesClient) {
-        this.sesClient = sesClient;
-    }
 
     public void createTemplate(NewTemplateRequestDto dto) {
         Template template = Template.builder()
