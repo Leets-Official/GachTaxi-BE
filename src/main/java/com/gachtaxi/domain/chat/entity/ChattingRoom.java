47697312fb23fb8f4102bcf15ac25429044e4f87
hyxklee <ewgt1234@naver.com>
2025-01-09 17:49:41 +0900

    feat: 채팅방 상태 추가

--- src/main/java/com/gachtaxi/domain/chat/entity/ChattingRoom.java
+++ src/main/java/com/gachtaxi/domain/chat/entity/ChattingRoom.java
@@ -1,15 +1,25 @@
 package com.gachtaxi.domain.chat.entity;
 
+import com.gachtaxi.domain.chat.entity.enums.Status;
 import com.gachtaxi.global.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @Entity
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class ChattingRoom extends BaseEntity {
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+
+    public void delete() {
+        status = Status.INACTIVE;
+    }
 }
