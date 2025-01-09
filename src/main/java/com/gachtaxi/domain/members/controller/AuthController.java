0a62629547082a583962d148a3073804c024bee9
koreaioi <koreaioi@gachon.ac.kr>
2025-01-09 15:41:05 +0900

    fix: 이메일 인증 위치 변경

--- src/main/java/com/gachtaxi/domain/members/controller/AuthController.java
+++ src/main/java/com/gachtaxi/domain/members/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.gachtaxi.domain.members.controller;
 
+import com.gachtaxi.domain.members.dto.request.InactiveMemberAuthCodeRequestDto;
+import com.gachtaxi.domain.members.dto.response.InactiveMemberResponseDto;
 import com.gachtaxi.domain.members.service.AuthService;
+import com.gachtaxi.domain.members.service.MemberService;
 import com.gachtaxi.global.auth.enums.OauthLoginStatus;
+import com.gachtaxi.global.auth.jwt.annotation.CurrentMemberId;
 import com.gachtaxi.global.auth.jwt.dto.JwtTokenDto;
 import com.gachtaxi.global.auth.jwt.service.JwtService;
+import com.gachtaxi.global.common.mail.dto.request.EmailAddressDto;
+import com.gachtaxi.global.common.mail.service.EmailService;
 import com.gachtaxi.global.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,14 +21,19 @@ import org.springframework.web.bind.annotation.*;
 import static com.gachtaxi.domain.members.controller.ResponseMessage.*;
 import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.KakaoAuthCode;
 import static com.gachtaxi.global.auth.kakao.dto.KaKaoDTO.OauthKakaoResponse;
+import static com.gachtaxi.global.common.mail.message.ResponseMessage.EMAIL_AUTHENTICATION_SUCESS;
+import static com.gachtaxi.global.common.mail.message.ResponseMessage.EMAIL_SEND_SUCCESS;
+import static org.springframework.http.HttpStatus.OK;
 
 @RequestMapping("/auth")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final EmailService emailService;
     private final AuthService authService;
     private final JwtService jwtService;
+    private final MemberService memberService;
 
     @PostMapping("/login/kakao")
     public ApiResponse<OauthKakaoResponse> kakaoLogin(@RequestBody @Valid KakaoAuthCode kakaoAuthCode, HttpServletResponse response) {
@@ -40,5 +51,24 @@ public class AuthController {
         jwtService.setCookie(jwtTokenDto.refreshToken(), response);
         jwtService.setHeader(jwtTokenDto.accessToken(), response);
         return ApiResponse.response(HttpStatus.OK, REFRESH_TOKEN_REISSUE.getMessage());
+    }
+
+    @PostMapping("/code/mail")
+    public ApiResponse sendEmail(
+            @RequestBody @Valid EmailAddressDto emailDto,
+            @CurrentMemberId Long userId
+    ) {
+        emailService.sendEmail(emailDto.email());
+        return ApiResponse.response(OK, EMAIL_SEND_SUCCESS.getMessage(), InactiveMemberResponseDto.from(userId));
+    }
+
+    @PatchMapping("/code/mail")
+    public ApiResponse checkAuthCodeAndUpdateEmail(
+            @RequestBody @Valid InactiveMemberAuthCodeRequestDto dto,
+            @CurrentMemberId Long userId
+    ) {
+        emailService.checkEmailAuthCode(dto.email(), dto.authCode());
+        memberService.updateInactiveMemberOfEmail(dto.email(), userId);
+        return ApiResponse.response(OK, EMAIL_AUTHENTICATION_SUCESS.getMessage(), InactiveMemberResponseDto.from(userId));
     }
 }
