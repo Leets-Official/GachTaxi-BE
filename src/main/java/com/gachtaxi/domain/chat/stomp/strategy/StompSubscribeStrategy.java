ca6e7dc9a1124753f67b0fca3ac616709a665fa1
hyxklee <ewgt1234@naver.com>
2025-01-09 15:26:16 +0900

    feat: STOMP sub 전략 수정

--- src/main/java/com/gachtaxi/domain/chat/stomp/strategy/StompSubscribeStrategy.java
+++ src/main/java/com/gachtaxi/domain/chat/stomp/strategy/StompSubscribeStrategy.java
@@ -1,16 +1,27 @@
 package com.gachtaxi.domain.chat.stomp.strategy;
 
 import com.gachtaxi.domain.chat.exception.ChatSubscribeException;
+import com.gachtaxi.domain.chat.service.ChattingRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 
+import static com.gachtaxi.domain.chat.stomp.strategy.StompConnectStrategy.CHAT_USER_ID;
+
 @Component
-public class StompSubscribeStrategy implements StompCommandStrategy{
+@RequiredArgsConstructor
+public class StompSubscribeStrategy implements StompCommandStrategy {
 
     private static final String SUB_END_POINT = "/sub/chat/room/";
+
+    private final ChattingRoomService chattingRoomService;
+
+    @Value("${chat.topic}")
+    public String chatTopic;
 
     @Override
     public boolean supports(StompCommand command) {
@@ -24,6 +35,12 @@ public class StompSubscribeStrategy implements StompCommandStrategy{
         if (!destination.startsWith(SUB_END_POINT)) {
             throw new ChatSubscribeException();
         }
+
+        Long senderId = (Long) accessor.getSessionAttributes().get(CHAT_USER_ID);
+        Long roomId = Long.valueOf(destination.replace(SUB_END_POINT, ""));
+        String senderName = accessor.getFirstNativeHeader("senderName");
+
+        chattingRoomService.subscribeChatRoom(roomId, senderId, senderName);
 
         return message;
     }
