c388570cb998bfd1c0f2a9827bed54f7a164e7e6
koreaioi <koreaioi@gachon.ac.kr>
2025-01-12 00:35:54 +0900

    fix: BaseEntity TimeStamp 적용

--- src/main/java/com/gachtaxi/GachtaxiApplication.java
+++ src/main/java/com/gachtaxi/GachtaxiApplication.java
@@ -2,9 +2,11 @@ package com.gachtaxi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableMongoAuditing
 public class GachtaxiApplication {
 
