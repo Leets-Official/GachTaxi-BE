25b6d01cbe34184b1393d43cd291ff4541e24439
senna <kjy915875@gmail.com>
2025-01-13 23:36:28 +0900

    feat: 컨트롤러가 current member id를 필요로하지만 파라미터로 받지 않았을 때 던질 예외 추가

--- src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
@@ -11,7 +11,8 @@ public enum ErrorMessage {
   NOT_ACTIVE_MATCHING_ROOM("열린 매칭 방이 아닙니다."),
   MEMBER_NOT_IN_MATCHING_ROOM("해당 매칭 방에 참가한 멤버가 아닙니다."),
   MEMBER_ALREADY_JOINED_MATCHING_ROOM("해당 맴버는 이미 매칭 방에 참가한 멤버입니다"),
-  MEMBER_ALREADY_LEFT_MATCHING_ROOM("해당 멤버는 이미 매칭 방에서 나간 멤버입니다.");
+  MEMBER_ALREADY_LEFT_MATCHING_ROOM("해당 멤버는 이미 매칭 방에서 나간 멤버입니다."),
+  CONTROLLER_NOT_HAS_CURRENT_MEMBER_ID("해당 컨트롤러는 인가된 멤버 ID가 필요합니다.");
 
   private final String message;
 }
