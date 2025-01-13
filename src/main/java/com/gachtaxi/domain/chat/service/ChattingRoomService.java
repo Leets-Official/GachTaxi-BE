de05aea9b1897b6c6c6a1f02fa23a43bbee1c77b
hyxklee <ewgt1234@naver.com>
2025-01-13 13:10:03 +0900

    feat: 채팅방 재입장 처리

--- src/main/java/com/gachtaxi/domain/chat/service/ChattingRoomService.java
+++ src/main/java/com/gachtaxi/domain/chat/service/ChattingRoomService.java
@@ -1,19 +1,26 @@
 package com.gachtaxi.domain.chat.service;
 
 import com.gachtaxi.domain.chat.dto.request.ChatMessage;
-import com.gachtaxi.domain.chat.dto.request.ChattingRoomResponse;
+import com.gachtaxi.domain.chat.dto.response.ChattingRoomResponse;
+import com.gachtaxi.domain.chat.entity.ChattingMessage;
+import com.gachtaxi.domain.chat.entity.ChattingParticipant;
 import com.gachtaxi.domain.chat.entity.ChattingRoom;
 import com.gachtaxi.domain.chat.entity.enums.Status;
 import com.gachtaxi.domain.chat.exception.ChattingRoomNotFoundException;
+import com.gachtaxi.domain.chat.exception.DuplicateSubscribeException;
 import com.gachtaxi.domain.chat.redis.RedisChatPublisher;
+import com.gachtaxi.domain.chat.repository.ChattingMessageRepository;
 import com.gachtaxi.domain.chat.repository.ChattingParticipantRepository;
 import com.gachtaxi.domain.chat.repository.ChattingRoomRepository;
+import com.gachtaxi.domain.members.entity.Members;
 import com.gachtaxi.domain.members.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +30,8 @@ public class ChattingRoomService {
 
     private final ChattingRoomRepository chattingRoomRepository;
     private final ChattingParticipantRepository chattingParticipantRepository;
+    private final ChattingMessageRepository chattingMessageRepository;
+    private final ChattingParticipantService chattingParticipantService;
     private final MemberService memberService;
     private final RedisChatPublisher redisChatPublisher;
 
@@ -47,20 +56,47 @@ public class ChattingRoomService {
     @Transactional
     public void subscribeChatRoom(long roomId, long senderId, String senderName) {
         ChattingRoom chattingRoom = find(roomId);
-//        Members members = memberService.find();
+        Members members = memberService.find(senderId);
 
-        ChannelTopic topic = new ChannelTopic(chatTopic + roomId);
-        ChatMessage chatMessage = ChatMessage.subscribe(roomId, senderId, senderName, senderName + ENTER_MESSAGE);
+        Optional<ChattingParticipant> optionalChattingParticipant = find(chattingRoom, members);
 
-//        ChattingParticipant chattingParticipant = ChattingParticipant.of(chattingRoom, members);
-//        chattingParticipantRepository.save(chattingParticipant);
+        if (optionalChattingParticipant.isPresent()) {
+            ChattingParticipant presentParticipant = optionalChattingParticipant.get();
 
-        redisChatPublisher.publish(topic, chatMessage);
+            checkDuplicateSubscription(presentParticipant);
+            presentParticipant.subscribe();
+
+            return;
+        }
+
+        ChattingParticipant newParticipant = ChattingParticipant.of(chattingRoom, members);
+        chattingParticipantService.save(newParticipant);
+
+        publishEnterMessage(roomId, senderId, senderName);
     }
 
     public ChattingRoom find(long chattingRoomId) {
         return chattingRoomRepository.findById(chattingRoomId)
                 .filter(chattingRoom -> chattingRoom.getStatus() == Status.ACTIVE)
                 .orElseThrow(ChattingRoomNotFoundException::new);
+    }
+
+    private void publishEnterMessage(long roomId, long senderId, String senderName) {
+        ChannelTopic topic = new ChannelTopic(chatTopic + roomId);
+        ChatMessage chatMessage = ChatMessage.subscribe(roomId, senderId, senderName, senderName + ENTER_MESSAGE);
+        ChattingMessage chattingMessage = ChattingMessage.from(chatMessage);
+
+        chattingMessageRepository.save(chattingMessage);
+        redisChatPublisher.publish(topic, chatMessage);
+    }
+
+    private Optional<ChattingParticipant> find(ChattingRoom chattingRoom, Members members) {
+        return chattingParticipantRepository.findByChattingRoomAndMembers(chattingRoom, members);
+    }
+
+    private void checkDuplicateSubscription(ChattingParticipant chattingParticipant) {
+        if (chattingParticipant.getStatus() == Status.ACTIVE) {
+            throw new DuplicateSubscribeException();
+        }
     }
 }
