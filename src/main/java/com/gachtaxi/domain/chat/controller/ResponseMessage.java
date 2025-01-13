250b7227cf2a11e510968fcb5c6dc7677e198622
hyxklee <ewgt1234@naver.com>
2025-01-13 13:52:46 +0900

    feat: 메시지 추가

--- src/main/java/com/gachtaxi/domain/chat/controller/ResponseMessage.java
+++ src/main/java/com/gachtaxi/domain/chat/controller/ResponseMessage.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ResponseMessage {
 
     CREATE_CHATTING_ROOM_SUCCESS("채팅방 생성에 성공했습니다."),
-    GET_CHATTING_MESSAGE_SUCCESS("이전 메시지 조회에 성공 했습니다.");
+    GET_CHATTING_MESSAGE_SUCCESS("이전 메시지 조회에 성공 했습니다."),
+    EXIT_CHATTING_ROOM_SUCCESS("채팅방 퇴장에 성공했습니다.");
 
     private final String message;
 }
