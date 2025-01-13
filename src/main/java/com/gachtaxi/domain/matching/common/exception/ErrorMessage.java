242b7e216f908d145060e027bc7b53f2bc69d69f
senna <kjy915875@gmail.com>
2025-01-13 23:18:15 +0900

    feat: MemberAlreadyLeftMatchingRoom 예외 추가

--- src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
@@ -10,7 +10,8 @@ public enum ErrorMessage {
   NO_SUCH_MATCHING_ROOM("해당 매칭 방이 존재하지 않습니다."),
   NOT_ACTIVE_MATCHING_ROOM("열린 매칭 방이 아닙니다."),
   MEMBER_NOT_IN_MATCHING_ROOM("해당 매칭 방에 참가한 멤버가 아닙니다."),
-  MEMBER_ALREADY_JOINED_MATCHING_ROOM("해당 맴버는 이미 매칭 방에 참가한 멤버입니다");
+  MEMBER_ALREADY_JOINED_MATCHING_ROOM("해당 맴버는 이미 매칭 방에 참가한 멤버입니다"),
+  MEMBER_ALREADY_LEFT_MATCHING_ROOM("해당 멤버는 이미 매칭 방에서 나간 멤버입니다.");
 
   private final String message;
 }
