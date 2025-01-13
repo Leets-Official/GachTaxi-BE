392584b8ea6f17ca0770a0467b28b8e039025752
hyxklee <ewgt1234@naver.com>
2025-01-13 13:09:47 +0900

    feat: 채팅 참여자 관련 공통 메서드 구현

---
+++ src/main/java/com/gachtaxi/domain/chat/service/ChattingParticipantService.java
@@ -0,0 +1,25 @@
+package com.gachtaxi.domain.chat.service;
+
+import com.gachtaxi.domain.chat.entity.ChattingParticipant;
+import com.gachtaxi.domain.chat.entity.ChattingRoom;
+import com.gachtaxi.domain.chat.exception.ChattingParticipantNotFoundException;
+import com.gachtaxi.domain.chat.repository.ChattingParticipantRepository;
+import com.gachtaxi.domain.members.entity.Members;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChattingParticipantService {
+
+    private final ChattingParticipantRepository chattingParticipantRepository;
+
+    public void save(ChattingParticipant chattingParticipant) {
+        chattingParticipantRepository.save(chattingParticipant);
+    }
+
+    public ChattingParticipant find(ChattingRoom chattingRoom, Members member) {
+        return chattingParticipantRepository.findByChattingRoomAndMembers(chattingRoom, member)
+                .orElseThrow(ChattingParticipantNotFoundException::new);
+    }
+}
