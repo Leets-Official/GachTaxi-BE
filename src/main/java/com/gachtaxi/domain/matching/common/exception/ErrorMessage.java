b0af0fea0171153698f9dfba1e645ff121562021
senna <kjy915875@gmail.com>
2025-01-14 03:08:39 +0900

    feat: NotDefienedKafkaTemplateException 추가

--- src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
@@ -12,7 +12,8 @@ public enum ErrorMessage {
   MEMBER_NOT_IN_MATCHING_ROOM("해당 매칭 방에 참가한 멤버가 아닙니다."),
   MEMBER_ALREADY_JOINED_MATCHING_ROOM("해당 맴버는 이미 매칭 방에 참가한 멤버입니다"),
   MEMBER_ALREADY_LEFT_MATCHING_ROOM("해당 멤버는 이미 매칭 방에서 나간 멤버입니다."),
-  CONTROLLER_NOT_HAS_CURRENT_MEMBER_ID("해당 컨트롤러는 인가된 멤버 ID가 필요합니다.");
+  CONTROLLER_NOT_HAS_CURRENT_MEMBER_ID("해당 컨트롤러는 인가된 멤버 ID가 필요합니다."),
+  NOT_DEFINED_KAFKA_TEMPLATE("해당 이벤트와 맞는 KafkaTemplate이 정의되지 않았습니다.");
 
   private final String message;
 }
