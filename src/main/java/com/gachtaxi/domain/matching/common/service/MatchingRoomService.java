6b6b5c09726835d4d11d86b767a0f22b8e55755d
senna <kjy915875@gmail.com>
2025-01-13 18:26:16 +0900

    feat: 매칭 취소 관련 비즈니스 로직 구현

--- src/main/java/com/gachtaxi/domain/matching/common/service/MatchingRoomService.java
+++ src/main/java/com/gachtaxi/domain/matching/common/service/MatchingRoomService.java
@@ -5,22 +5,31 @@ import com.gachtaxi.domain.matching.common.entity.MatchingRoomTagInfo;
 import com.gachtaxi.domain.matching.common.entity.MemberMatchingRoomChargingInfo;
 import com.gachtaxi.domain.matching.common.entity.Route;
 import com.gachtaxi.domain.matching.common.entity.enums.MatchingRoomStatus;
+import com.gachtaxi.domain.matching.common.entity.enums.PaymentStatus;
 import com.gachtaxi.domain.matching.common.entity.enums.Tags;
+import com.gachtaxi.domain.matching.common.exception.MemberAlreadyJoinedException;
+import com.gachtaxi.domain.matching.common.exception.MemberNotInMatchingRoomException;
 import com.gachtaxi.domain.matching.common.exception.NoSuchMatchingRoomException;
 import com.gachtaxi.domain.matching.common.exception.NotActiveMatchingRoomException;
 import com.gachtaxi.domain.matching.common.repository.MatchingRoomRepository;
 import com.gachtaxi.domain.matching.common.repository.MatchingRoomTagInfoRepository;
 import com.gachtaxi.domain.matching.common.repository.MemberMatchingRoomChargingInfoRepository;
 import com.gachtaxi.domain.matching.common.repository.RouteRepository;
+import com.gachtaxi.domain.matching.event.dto.kafka_topic.MatchMemberCancelledEvent;
 import com.gachtaxi.domain.matching.event.dto.kafka_topic.MatchMemberJoinedEvent;
+import com.gachtaxi.domain.matching.event.dto.kafka_topic.MatchRoomCancelledEvent;
 import com.gachtaxi.domain.matching.event.dto.kafka_topic.MatchRoomCreatedEvent;
+import com.gachtaxi.domain.matching.event.service.kafka.AutoMatchingProducer;
 import com.gachtaxi.domain.members.entity.Members;
 import com.gachtaxi.domain.members.service.MemberService;
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -28,6 +37,7 @@ public class MatchingRoomService {
 
   // service
   private final MemberService memberService;
+  private final AutoMatchingProducer autoMatchingProducer;
 
   // repository
   private final MatchingRoomRepository matchingRoomRepository;
@@ -83,6 +93,7 @@ public class MatchingRoomService {
         .matchingRoom(matchingRoom)
         .members(members)
         .charge(matchingRoom.getTotalCharge())
+        .paymentStatus(PaymentStatus.NOT_PAYED)
         .build();
 
     this.memberMatchingRoomChargingInfoRepository.save(matchingRoomChargingInfo);
@@ -93,6 +104,10 @@ public class MatchingRoomService {
     MatchingRoom matchingRoom = this.matchingRoomRepository.findById(
         matchMemberJoinedEvent.roomId()).orElseThrow(
         NoSuchMatchingRoomException::new);
+
+    if (this.memberMatchingRoomChargingInfoRepository.findByMembersAndMatchingRoom(members, matchingRoom).isPresent()) {
+      throw new MemberAlreadyJoinedException();
+    }
 
     if (!matchingRoom.isActiveMatchingRoom()) {
       throw new NotActiveMatchingRoomException();
@@ -109,6 +124,7 @@ public class MatchingRoomService {
             .matchingRoom(matchingRoom)
             .members(members)
             .charge(distributedCharge)
+            .paymentStatus(PaymentStatus.NOT_PAYED)
             .build()
     );
 
@@ -120,5 +136,49 @@ public class MatchingRoomService {
       memberMatchingRoomChargingInfo.setCharge(charge);
     }
     this.memberMatchingRoomChargingInfoRepository.saveAll(existMembers);
+  }
+
+  public void leaveMemberFromMatchingRoom(MatchMemberCancelledEvent matchMemberCancelledEvent) {
+    Members members = this.memberService.findById(matchMemberCancelledEvent.memberId());
+    MatchingRoom matchingRoom = this.matchingRoomRepository.findById(
+        matchMemberCancelledEvent.roomId()).orElseThrow(
+        NoSuchMatchingRoomException::new);
+
+    MemberMatchingRoomChargingInfo memberMatchingRoomChargingInfo = this.memberMatchingRoomChargingInfoRepository.findByMembersAndMatchingRoom(
+            members, matchingRoom)
+        .orElseThrow(MemberNotInMatchingRoomException::new);
+
+    memberMatchingRoomChargingInfo.leftMatchingRoom();
+
+    this.memberMatchingRoomChargingInfoRepository.save(memberMatchingRoomChargingInfo);
+
+    if (members.isRoomMaster(matchingRoom)) {
+      this.findNextRoomMaster(matchingRoom, members)
+          .ifPresentOrElse(
+              nextRoomMaster -> matchingRoom.changeRoomMaster(nextRoomMaster),
+              () -> this.autoMatchingProducer.sendMatchRoomCancelledEvent(
+                  MatchRoomCancelledEvent.builder().roomId(matchingRoom.getId()).build()
+              )
+          );
+    }
+  }
+
+  private Optional<Members> findNextRoomMaster(MatchingRoom matchingRoom, Members members) {
+    List<MemberMatchingRoomChargingInfo> existMembers = this.memberMatchingRoomChargingInfoRepository.findByMatchingRoom(
+        matchingRoom);
+
+    return existMembers.stream()
+        .map(MemberMatchingRoomChargingInfo::getMembers)
+        .filter(member -> !member.equals(members))
+        .findFirst();
+  }
+
+  public void cancelMatchingRoom(MatchRoomCancelledEvent matchRoomCancelledEvent) {
+    MatchingRoom matchingRoom = this.matchingRoomRepository.findById(
+        matchRoomCancelledEvent.roomId()).orElseThrow(
+        NoSuchMatchingRoomException::new);
+
+    matchingRoom.cancelMatchingRoom();
+    this.matchingRoomRepository.save(matchingRoom);
   }
 }
