2b31cdb56516a5737a6f98a03123fe40b9140f10
senna <kjy915875@gmail.com>
2025-01-13 23:19:41 +0900

    refactor: 멤버가 참여한 매칭방을 조회할때 PaymentStatus도 확인하도록 수정

--- src/main/java/com/gachtaxi/domain/matching/common/repository/MemberMatchingRoomChargingInfoRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/MemberMatchingRoomChargingInfoRepository.java
@@ -2,6 +2,7 @@ package com.gachtaxi.domain.matching.common.repository;
 
 import com.gachtaxi.domain.matching.common.entity.MatchingRoom;
 import com.gachtaxi.domain.matching.common.entity.MemberMatchingRoomChargingInfo;
+import com.gachtaxi.domain.matching.common.entity.enums.PaymentStatus;
 import com.gachtaxi.domain.members.entity.Members;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MemberMatchingRoomChargingInfoRepository extends JpaRepository<MemberMatchingRoomChargingInfo, Long> {
 
-  List<MemberMatchingRoomChargingInfo> findByMatchingRoom(MatchingRoom matchingRoom);
+  List<MemberMatchingRoomChargingInfo> findByMatchingRoomAAndPaymentStatus(MatchingRoom matchingRoom, PaymentStatus paymentStatus);
   Optional<MemberMatchingRoomChargingInfo> findByMembersAndMatchingRoom(Members members, MatchingRoom matchingRoom);
 }
