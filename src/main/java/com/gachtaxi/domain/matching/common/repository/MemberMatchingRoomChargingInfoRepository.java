d5c389dc6c8267b8a9cb70bafde76e2696e44cd0
senna <kjy915875@gmail.com>
2025-01-13 18:28:25 +0900

    feat: MemberMatchingRoomChargingInfoRepository.findByMembersAndMatchinRoom 메서드 추가

--- src/main/java/com/gachtaxi/domain/matching/common/repository/MemberMatchingRoomChargingInfoRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/MemberMatchingRoomChargingInfoRepository.java
@@ -2,7 +2,9 @@ package com.gachtaxi.domain.matching.common.repository;
 
 import com.gachtaxi.domain.matching.common.entity.MatchingRoom;
 import com.gachtaxi.domain.matching.common.entity.MemberMatchingRoomChargingInfo;
+import com.gachtaxi.domain.members.entity.Members;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ import org.springframework.stereotype.Repository;
 public interface MemberMatchingRoomChargingInfoRepository extends JpaRepository<MemberMatchingRoomChargingInfo, Long> {
 
   List<MemberMatchingRoomChargingInfo> findByMatchingRoom(MatchingRoom matchingRoom);
+  Optional<MemberMatchingRoomChargingInfo> findByMembersAndMatchingRoom(Members members, MatchingRoom matchingRoom);
 }
