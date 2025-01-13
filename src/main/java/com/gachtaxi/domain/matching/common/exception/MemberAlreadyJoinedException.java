a2d7000c5e5b39862709eb70397d5550c27173a3
senna <kjy915875@gmail.com>
2025-01-13 19:50:35 +0900

    refactor: 예외들에 정적 변수 추가

--- src/main/java/com/gachtaxi/domain/matching/common/exception/MemberAlreadyJoinedException.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/MemberAlreadyJoinedException.java
@@ -1,11 +1,13 @@
 package com.gachtaxi.domain.matching.common.exception;
 
+import static com.gachtaxi.domain.matching.common.exception.ErrorMessage.MEMBER_ALREADY_JOINED_MATCHING_ROOM;
+import static org.springframework.http.HttpStatus.CONFLICT;
+
 import com.gachtaxi.global.common.exception.BaseException;
-import org.springframework.http.HttpStatus;
 
 public class MemberAlreadyJoinedException extends BaseException {
 
   public MemberAlreadyJoinedException() {
-    super(HttpStatus.CONFLICT, ErrorMessage.MEMBER_ALREADY_JOINED_MATCHING_ROOM.getMessage());
+    super(CONFLICT, MEMBER_ALREADY_JOINED_MATCHING_ROOM.getMessage());
   }
 }
--- src/main/java/com/gachtaxi/domain/matching/common/exception/MemberNotInMatchingRoomException.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/MemberNotInMatchingRoomException.java
@@ -1,11 +1,13 @@
 package com.gachtaxi.domain.matching.common.exception;
 
+import static com.gachtaxi.domain.matching.common.exception.ErrorMessage.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import com.gachtaxi.global.common.exception.BaseException;
-import org.springframework.http.HttpStatus;
 
 public class MemberNotInMatchingRoomException extends BaseException {
 
   public MemberNotInMatchingRoomException() {
-    super(HttpStatus.BAD_REQUEST, ErrorMessage.MEMBER_NOT_IN_MATCHING_ROOM.getMessage());
+    super(BAD_REQUEST, MEMBER_NOT_IN_MATCHING_ROOM.getMessage());
   }
 }
