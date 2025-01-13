56fab24942f97880b0365368dbe55b7d74514fab
hyxklee <ewgt1234@naver.com>
2025-01-13 13:09:21 +0900

    feat: 참여 상태 및 참여일, 연결 해제 일 추가

--- src/main/java/com/gachtaxi/domain/chat/entity/ChattingParticipant.java
+++ src/main/java/com/gachtaxi/domain/chat/entity/ChattingParticipant.java
@@ -1,14 +1,17 @@
 package com.gachtaxi.domain.chat.entity;
 
+import com.gachtaxi.domain.chat.entity.enums.Status;
 import com.gachtaxi.domain.members.entity.Members;
 import com.gachtaxi.global.common.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -24,10 +27,32 @@ public class ChattingParticipant extends BaseEntity {
     @JoinColumn(name = "members_id")
     private Members members;
 
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime joinedAt;
+
+    private LocalDateTime disconnectedAt;
+
     public static ChattingParticipant of(ChattingRoom chattingRoom, Members members) {
         return ChattingParticipant.builder()
                 .chattingRoom(chattingRoom)
                 .members(members)
                 .build();
+    }
+
+    public void subscribe() {
+        this.status = Status.ACTIVE;
+    }
+
+    /*
+    todo disconnect 이벤트 처리 시 요청하기
+     */
+    public void disconnect(Status status) {
+        this.status = status;
+        this.disconnectedAt = LocalDateTime.now();
     }
 }
