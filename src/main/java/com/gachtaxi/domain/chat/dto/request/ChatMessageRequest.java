1dfdac975cf35dc7c8e8ab64fb09dc808e589b55
hyxklee <ewgt1234@naver.com>
2025-01-14 14:13:35 +0900

    refactor: notblank 추가

--- src/main/java/com/gachtaxi/domain/chat/dto/request/ChatMessageRequest.java
+++ src/main/java/com/gachtaxi/domain/chat/dto/request/ChatMessageRequest.java
@@ -1,6 +1,8 @@
 package com.gachtaxi.domain.chat.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ChatMessageRequest(
-        String message
+        @NotBlank String message
 ) {
 }
