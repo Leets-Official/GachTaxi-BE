b305484a3c18846bca535e737796518a7eaf9698
huncozyboy <leejh5838@naver.com>
2025-01-16 22:48:22 +0900

    feat: 매칭 출발지와 도착지 좌표, 태그 조건으로 필터링 메서드 구현

--- src/main/java/com/gachtaxi/domain/matching/algorithm/service/MockMatchingAlgorithmService.java
+++ src/main/java/com/gachtaxi/domain/matching/algorithm/service/MockMatchingAlgorithmService.java
@@ -4,6 +4,9 @@ import com.gachtaxi.domain.matching.algorithm.dto.FindRoomResult;
 import com.gachtaxi.domain.matching.common.entity.MatchingRoom;
 import com.gachtaxi.domain.matching.common.entity.enums.Tags;
 import com.gachtaxi.domain.matching.common.repository.MatchingRoomRepository;
+import com.gachtaxi.domain.members.entity.Members;
+import com.gachtaxi.domain.members.exception.MemberNotFoundException;
+import com.gachtaxi.domain.members.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -14,18 +17,46 @@ import org.springframework.stereotype.Service;
 public class MockMatchingAlgorithmService implements MatchingAlgorithmService {
 
   private final MatchingRoomRepository matchingRoomRepository;
+  private final MemberRepository memberRepository;
 
   @Override
-  public Optional<FindRoomResult> findRoom(Long userId, String startPoint, String destinationPoint, List<Tags> criteria) {
-    List<MatchingRoom> matchingRoomList = this.matchingRoomRepository.findAll();
-    if (!matchingRoomList.isEmpty()) {
-      MatchingRoom matchingRoom = matchingRoomList.get(0);
-      return Optional.of(
-          FindRoomResult.builder()
-              .roomId(matchingRoom.getId())
-              .maxCapacity(matchingRoom.getCapacity())
-              .build());
+  public Optional<FindRoomResult> findRoom(Long userId, double startLongitude, double startLatitude, double destinationLongitude, double destinationLatitude,
+                                           List<Tags> criteria) {
+    /*
+     사용자 ID로 사용자 정보 조회(이미 방에 참여하고 있는지 중복체크)
+     */
+    Members user = memberRepository.findById(userId)
+            .orElseThrow(MemberNotFoundException::new);
+
+    boolean isAlreadyInRoom = matchingRoomRepository.existsByMemberInMatchingRoom(user);
+    if (isAlreadyInRoom) {
+      return Optional.empty();
     }
+    /*
+     출발지 기준 300m 이내 + 태그 조건에 맞는 방 검색
+     */
+    List<MatchingRoom> matchingRooms = matchingRoomRepository.findRoomsByStartAndDestinationAndTags(
+            startLongitude,
+            startLatitude,
+            destinationLongitude,
+            destinationLatitude,
+            criteria
+    );
+    /*
+     조건에 맞는 방이 있으면 첫 번째 방의 상세 정보 반환
+     */
+    if (!matchingRooms.isEmpty()) {
+      MatchingRoom room = matchingRooms.get(0);
+      return Optional.of(
+              FindRoomResult.builder()
+                      .roomId(room.getId())
+                      .maxCapacity(room.getCapacity())
+                      .build()
+      );
+    }
+    /*
+     조건에 맞는 방이 없으면 empty 반환
+     */
     return Optional.empty();
   }
 }
