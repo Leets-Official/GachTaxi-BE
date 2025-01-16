d796daa036ae00c1e069547264305b0ed411d37f
huncozyboy <leejh5838@naver.com>
2025-01-16 22:42:30 +0900

    refactor: 300m 이내 반경 지정 위해 위도 경도 정보 사용하도록 설정

--- src/main/java/com/gachtaxi/domain/matching/algorithm/service/MatchingAlgorithmService.java
+++ src/main/java/com/gachtaxi/domain/matching/algorithm/service/MatchingAlgorithmService.java
@@ -11,10 +11,12 @@ public interface MatchingAlgorithmService {
    * 방을 찾는 메서드
    * 이미 방에 들어가있는 멤버가 다시 요청했을 때 Optional.empty()를 반환하도록 로직을 구성해야함
    * @param userId 방에 들어가려는 사용자 ID
-   * @param startPoint 매칭 시작 지점 좌표
-   * @param destinationPoint 도착지 좌표
+   * @param startLongitude 시작 지점 경도
+   * @param startLatitude 시작 지점 위도
+   * @param destinationLongitude 도착 지점 경도
+   * @param destinationLatitude 도착 지점 위도
    * @param criteria 방 검색에 필요한 기타 조건 (태그 등)
    * @return Optional<FindRoomResult> - 매칭 가능한 방 정보가 있으면 값이 있고, 없으면 empty
    */
-  Optional<FindRoomResult> findRoom(Long userId, String startPoint, String destinationPoint, List<Tags> criteria);
+  Optional<FindRoomResult> findRoom(Long userId, double startLongitude, double startLatitude, double destinationLongitude, double destinationLatitude, List<Tags> criteria);
 }
