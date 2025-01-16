103f446b984ccd3ef40b40c939121bed7f3f58dd
huncozyboy <leejh5838@naver.com>
2025-01-16 22:44:18 +0900

    feat: 매칭방 리스트 검색 쿼리 및 중복 참여 방지 쿼리 구현

--- src/main/java/com/gachtaxi/domain/matching/common/repository/MatchingRoomRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/MatchingRoomRepository.java
@@ -1,10 +1,33 @@
 package com.gachtaxi.domain.matching.common.repository;
 
 import com.gachtaxi.domain.matching.common.entity.MatchingRoom;
+import com.gachtaxi.domain.matching.common.entity.enums.Tags;
+import com.gachtaxi.domain.members.entity.Members;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MatchingRoomRepository extends JpaRepository<MatchingRoom, Long> {
+    @Query("SELECT r FROM MatchingRoom r " +
+            "JOIN r.matchingRoomTagInfo ti " +
+            "WHERE " +
+            "FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :startLongitude, :startLatitude), FUNCTION('POINT', r.route.startLongitude, r.route.startLatitude)) <= 300 " +
+            "AND FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :destinationLongitude, :destinationLatitude), FUNCTION('POINT', r.route.endLongitude, r.route.endLatitude)) <= 300 " +
+            "AND ti.tags IN (:tags) " +
+            "AND r.matchingRoomStatus = 'ACTIVE'")
+    List<MatchingRoom> findRoomsByStartAndDestinationAndTags(
+            @Param("startLongitude") double startLongitude,
+            @Param("startLatitude") double startLatitude,
+            @Param("destinationLongitude") double destinationLongitude,
+            @Param("destinationLatitude") double destinationLatitude,
+            @Param("tags") List<Tags> tags
+    );
 
+    @Query("SELECT CASE WHEN COUNT(m) > 0 THEN true ELSE false END " +
+            "FROM MatchingRoom r JOIN r.memberMatchingRoomChargingInfo m " +
+            "WHERE m.members = :user")
+    boolean existsByMemberInMatchingRoom(@Param("user") Members user);
 }
