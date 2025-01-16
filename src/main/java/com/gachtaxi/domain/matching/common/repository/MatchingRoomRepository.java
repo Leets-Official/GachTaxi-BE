a2903835795bceca5d3e762d0ed1f07ac134e65e
huncozyboy <leejh5838@naver.com>
2025-01-17 04:56:50 +0900

    refactor: 레포에서는 거리와 관련된 정보만 조회해오도록 쿼리문 수정

--- src/main/java/com/gachtaxi/domain/matching/common/repository/MatchingRoomRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/MatchingRoomRepository.java
@@ -1,7 +1,6 @@
 package com.gachtaxi.domain.matching.common.repository;
 
 import com.gachtaxi.domain.matching.common.entity.MatchingRoom;
-import com.gachtaxi.domain.matching.common.entity.enums.Tags;
 import com.gachtaxi.domain.members.entity.Members;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,25 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MatchingRoomRepository extends JpaRepository<MatchingRoom, Long> {
     @Query("SELECT r FROM MatchingRoom r " +
-            "JOIN r.matchingRoomTagInfo ti " +
             "WHERE " +
             "FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :startLongitude, :startLatitude), FUNCTION('POINT', r.route.startLongitude, r.route.startLatitude)) <= 300 " +
-            "AND FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :destinationLongitude, :destinationLatitude), FUNCTION('POINT', r.route.endLongitude, r.route.endLatitude)) <= 300 " +
-            "AND ti.tags IN (:tags) " +
-            "AND r.matchingRoomStatus = 'ACTIVE'")
-    List<MatchingRoom> findRoomsByStartAndDestinationAndTags(
-            @Param("startLongitude") double startLongitude,
-            @Param("startLatitude") double startLatitude,
-            @Param("destinationLongitude") double destinationLongitude,
-            @Param("destinationLatitude") double destinationLatitude,
-            @Param("tags") List<Tags> tags
-    );
-
-    @Query("SELECT r FROM MatchingRoom r " +
-            "WHERE " +
-            "FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :startLongitude, :startLatitude), FUNCTION('POINT', r.route.startLongitude, r.route.startLatitude)) <= 300 " +
-            "AND FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :destinationLongitude, :destinationLatitude), FUNCTION('POINT', r.route.endLongitude, r.route.endLatitude)) <= 300 " +
-            "AND r.matchingRoomStatus = 'ACTIVE'")
+            "AND FUNCTION('ST_Distance_Sphere', FUNCTION('POINT', :destinationLongitude, :destinationLatitude), FUNCTION('POINT', r.route.endLongitude, r.route.endLatitude)) <= 300 ")
     List<MatchingRoom> findRoomsByStartAndDestination(
             @Param("startLongitude") double startLongitude,
             @Param("startLatitude") double startLatitude,
