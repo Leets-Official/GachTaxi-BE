f29a3ba530680f96ae6866ef0d2536d5eb07a8cc
huncozyboy <leejh5838@naver.com>
2025-01-17 00:21:14 +0900

    feat: 페이지 예외 메시지 구현

--- src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorMessage {
 
   NO_SUCH_MATCHING_ROOM("해당 매칭 방이 존재하지 않습니다."),
-  NOT_ACTIVE_MATCHING_ROOM("열린 매칭 방이 아닙니다.");
+  NOT_ACTIVE_MATCHING_ROOM("열린 매칭 방이 아닙니다."),
+  NOT_FOUND_PAGE("페이지 번호는 0 이상이어야 합니다.");
 
   private final String message;
 }
