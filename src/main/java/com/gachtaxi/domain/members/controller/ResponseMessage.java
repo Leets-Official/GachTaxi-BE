7c34c90c8f7dfe12b51f79b4aa2f38085a93d266
hyxklee <ewgt1234@naver.com>
2025-01-17 14:20:52 +0900

    feat: 응답 메시지 추가

--- src/main/java/com/gachtaxi/domain/members/controller/ResponseMessage.java
+++ src/main/java/com/gachtaxi/domain/members/controller/ResponseMessage.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ResponseMessage {
     // MemberController
     REGISTER_SUCCESS("회원가입에 성공했습니다."),
+    FCM_TOKEN_UPDATE_SUCCESS("FCM 토큰 업데이트에 성공했습니다."),
 
     // AuthController
     REFRESH_TOKEN_REISSUE("토큰 재발급에 성공했습니다."),
