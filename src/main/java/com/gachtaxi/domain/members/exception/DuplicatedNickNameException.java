de727bc1b360762458b8c824615454fc92a13d11
koreaioi <koreaioi@gachon.ac.kr>
2025-01-18 09:55:19 +0900

    fix: 정적 경로 설정

--- src/main/java/com/gachtaxi/domain/members/exception/DuplicatedNickNameException.java
+++ src/main/java/com/gachtaxi/domain/members/exception/DuplicatedNickNameException.java
@@ -1,12 +1,12 @@
 package com.gachtaxi.domain.members.exception;
 
 import com.gachtaxi.global.common.exception.BaseException;
-import org.springframework.http.HttpStatus;
 
 import static com.gachtaxi.domain.members.exception.ErrorMessage.DUPLICATED_NICKNAME;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class DuplicatedNickNameException extends BaseException {
     public DuplicatedNickNameException() {
-        super(HttpStatus.BAD_REQUEST, DUPLICATED_NICKNAME.getMessage());
+        super(BAD_REQUEST, DUPLICATED_NICKNAME.getMessage());
     }
 }
