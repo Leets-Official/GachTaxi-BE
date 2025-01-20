8fd210ad2dd689a01e402482f320532c0c624efa
hyxklee <ewgt1234@naver.com>
2025-01-20 13:56:13 +0900

    refactor: 컬럼 추가 및 팩토리 메서드 수정

--- src/main/java/com/gachtaxi/domain/chat/dto/request/ChatMessage.java
+++ src/main/java/com/gachtaxi/domain/chat/dto/request/ChatMessage.java
@@ -1,5 +1,7 @@
 package com.gachtaxi.domain.chat.dto.request;
 
+import com.gachtaxi.domain.chat.dto.response.ReadMessageRange;
+import com.gachtaxi.domain.chat.entity.ChattingMessage;
 import com.gachtaxi.domain.chat.entity.enums.MessageType;
 import lombok.Builder;
 
@@ -7,30 +9,35 @@ import java.time.LocalDateTime;
 
 @Builder
 public record ChatMessage(
+        String messageId,
         Long roomId,
         Long senderId,
         String senderName,
         String message,
+        ReadMessageRange range,
+        Long unreadCount,
         LocalDateTime timeStamp,
         MessageType messageType
 ) {
-    public static ChatMessage of(ChatMessageRequest request, long roomId, long senderId, String senderName) {
+    public static ChatMessage from(ChattingMessage chattingMessage) {
         return ChatMessage.builder()
-                .roomId(roomId)
-                .senderId(senderId)
-                .senderName(senderName)
-                .message(request.message())
-                .timeStamp(LocalDateTime.now())
-                .messageType(MessageType.MESSAGE)
+                .messageId(chattingMessage.getId())
+                .roomId(chattingMessage.getRoomId())
+                .senderId(chattingMessage.getSenderId())
+                .senderName(chattingMessage.getSenderName())
+                .message(chattingMessage.getMessage())
+                .unreadCount(chattingMessage.getUnreadCount())
+                .timeStamp(chattingMessage.getTimeStamp())
+                .messageType(chattingMessage.getMessageType())
                 .build();
     }
 
-    public static ChatMessage of(long roomId, Long senderId, String senderName, String message, MessageType messageType) {
+    public static ChatMessage of(long roomId, Long senderId, String senderName, ReadMessageRange range, MessageType messageType) {
         return ChatMessage.builder()
                 .roomId(roomId)
                 .senderId(senderId)
                 .senderName(senderName)
-                .message(message)
+                .range(range)
                 .timeStamp(LocalDateTime.now())
                 .messageType(messageType)
                 .build();
