3ad5aa05f0178fa0f3db9d87da1c11ace0cd9ee1
senna <kjy915875@gmail.com>
2025-01-20 20:32:09 +0900

    fix: 세미콜론 수정

--- src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
+++ src/main/java/com/gachtaxi/domain/matching/common/exception/ErrorMessage.java
@@ -13,7 +13,7 @@ public enum ErrorMessage {
   MEMBER_ALREADY_JOINED_MATCHING_ROOM("해당 맴버는 이미 매칭 방에 참가한 멤버입니다"),
   MEMBER_ALREADY_LEFT_MATCHING_ROOM("해당 멤버는 이미 매칭 방에서 나간 멤버입니다."),
   CONTROLLER_NOT_HAS_CURRENT_MEMBER_ID("해당 컨트롤러는 인가된 멤버 ID가 필요합니다."),
-  NOT_DEFINED_KAFKA_TEMPLATE("해당 이벤트와 맞는 KafkaTemplate이 정의되지 않았습니다.");
+  NOT_DEFINED_KAFKA_TEMPLATE("해당 이벤트와 맞는 KafkaTemplate이 정의되지 않았습니다."),
   DUPLICATED_MATCHING_ROOM("이미 존재하는 매칭 방입니다."),
   NOT_FOUND_PAGE("페이지 번호는 0 이상이어야 합니다.");
 
