46b90bbf3645951eac325f8a02419395ef677fb2
hyxklee <ewgt1234@naver.com>
2025-01-20 13:57:29 +0900

    feat: 공통 메서드 분리 및 실시간 참여자 숫자 반환 메서드 구현

--- src/main/java/com/gachtaxi/domain/chat/service/ChatRedisService.java
+++ src/main/java/com/gachtaxi/domain/chat/service/ChatRedisService.java
@@ -1,5 +1,6 @@
 package com.gachtaxi.domain.chat.service;
 
+import com.gachtaxi.domain.chat.exception.ChattingRoomNotFoundException;
 import com.gachtaxi.domain.chat.exception.UnSubscriptionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,19 +14,19 @@ public class ChatRedisService {
     private final RedisTemplate<String, Object> chatRoomRedisTemplate;
 
     public void saveSubscribeMember(long roomId, long senderId) {
-        String key = REDIS_CHAT_KEY_PREFIX + roomId;
+        String key = getKey(roomId);
 
         chatRoomRedisTemplate.opsForSet().add(key, senderId);
     }
 
     public boolean isActive(long roomId, long senderId) {
-        String key = REDIS_CHAT_KEY_PREFIX + roomId;
+        String key = getKey(roomId);
 
         return Boolean.TRUE.equals(chatRoomRedisTemplate.opsForSet().isMember(key, senderId));
     }
 
     public void removeSubscribeMember(long roomId, long senderId) {
-        String key = REDIS_CHAT_KEY_PREFIX + roomId;
+        String key = getKey(roomId);
 
         chatRoomRedisTemplate.opsForSet().remove(key, senderId);
     }
@@ -34,5 +35,21 @@ public class ChatRedisService {
         if (!isActive(roomId, senderId)) {
             throw new UnSubscriptionException();
         }
+    }
+
+    public long getSubscriberCount(long roomId) {
+        String key = getKey(roomId);
+
+        Long size = chatRoomRedisTemplate.opsForSet().size(key);
+
+        if (size == null) {
+            throw new ChattingRoomNotFoundException();
+        }
+
+        return size;
+    }
+
+    private String getKey(long roomId) {
+        return REDIS_CHAT_KEY_PREFIX + roomId;
     }
 }
