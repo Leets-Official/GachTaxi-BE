4d8cc92938e310f34728c5bffdf855d76beebbc2
koreaioi <koreaioi@gachon.ac.kr>
2025-01-28 23:36:18 +0900

    feat: 친구 목록에 성별 추가

--- src/main/java/com/gachtaxi/domain/friend/dto/response/FriendsResponseDto.java
+++ src/main/java/com/gachtaxi/domain/friend/dto/response/FriendsResponseDto.java
@@ -1,19 +1,22 @@
 package com.gachtaxi.domain.friend.dto.response;
 
 import com.gachtaxi.domain.members.entity.Members;
+import com.gachtaxi.domain.members.entity.enums.Gender;
 import lombok.Builder;
 
 @Builder
 public record FriendsResponseDto(
         Long friendsId,
         String friendsNickName,
-        String friendsProfileUrl
+        String friendsProfileUrl,
+        Gender gender
 ) {
     public static FriendsResponseDto from(Members friends) {
         return FriendsResponseDto.builder()
                 .friendsId(friends.getId())
                 .friendsNickName(friends.getNickname())
                 .friendsProfileUrl(friends.getProfilePicture())
+                .gender(friends.getGender())
                 .build();
     }
 }
