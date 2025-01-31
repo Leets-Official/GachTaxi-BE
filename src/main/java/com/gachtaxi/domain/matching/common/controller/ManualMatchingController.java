e1eda47a6723df26252066fb97699ae4e6afdcbc
huncozyboy <leejh5838@naver.com>
2025-01-31 23:02:48 +0900

    refactor: CurrentMemberId 사용 및 RESTful 한 네이밍으로 변경

--- src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
@@ -5,9 +5,9 @@ import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.JOI
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.LEAVE_MANUAL_MATCHING_ROOM_SUCCESS;
 
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingJoinRequest;
-import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingLeaveRequest;
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingRequest;
 import com.gachtaxi.domain.matching.common.service.ManualMatchingService;
+import com.gachtaxi.global.auth.jwt.annotation.CurrentMemberId;
 import com.gachtaxi.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,6 +15,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +32,7 @@ public class ManualMatchingController {
     private final ManualMatchingService manualMatchingService;
 
     @Operation(summary = "수동 매칭방 생성")
-    @PostMapping("/create")
+    @PostMapping
     public ApiResponse<Long> createManualMatchingRoom(@Valid @RequestBody ManualMatchingRequest request) {
         Long roomId = manualMatchingService.createManualMatchingRoom(request);
         return ApiResponse.response(HttpStatus.OK, CREATE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage(), roomId);
@@ -38,15 +40,15 @@ public class ManualMatchingController {
 
     @Operation(summary = "수동 매칭방 참여")
     @PostMapping("/join")
-    public ApiResponse<Void> joinManualMatchingRoom(@Valid @RequestBody ManualMatchingJoinRequest request) {
-        manualMatchingService.joinManualMatchingRoom(request.userId(), request.roomId());
+    public ApiResponse<Void> joinManualMatchingRoom(@CurrentMemberId Long userId, @Valid @RequestBody ManualMatchingJoinRequest request) {
+        manualMatchingService.joinManualMatchingRoom(userId, request.roomId());
         return ApiResponse.response(HttpStatus.OK, JOIN_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
     }
 
     @Operation(summary = "수동 매칭방 퇴장 (방 삭제 포함)")
-    @PostMapping("/leave")
-    public ApiResponse<Void> leaveManualMatchingRoom(@Valid @RequestBody ManualMatchingLeaveRequest request) {
-        manualMatchingService.leaveManualMatchingRoom(request.userId(), request.roomId());
+    @PatchMapping("/exit/{roomId}")
+    public ApiResponse<Void> leaveManualMatchingRoom(@CurrentMemberId Long userId, @PathVariable Long roomId) {
+        manualMatchingService.leaveManualMatchingRoom(userId, roomId);
         return ApiResponse.response(HttpStatus.OK, LEAVE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
     }
 }
