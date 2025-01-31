2e117ca0b01c8a29cadc5d4938cf4ceeecd92418
huncozyboy <leejh5838@naver.com>
2025-01-31 21:40:11 +0900

    feat: 수동 매칭방 참여 컨트롤러 구현

--- src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
@@ -1,6 +1,9 @@
 package com.gachtaxi.domain.matching.common.controller;
 
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.CREATE_MANUAL_MATCHING_ROOM_SUCCESS;
+import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.JOIN_MANUAL_MATCHING_ROOM_SUCCESS;
+
+import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingJoinRequest;
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingRequest;
 import com.gachtaxi.domain.matching.common.service.ManualMatchingService;
 import com.gachtaxi.global.common.response.ApiResponse;
@@ -31,4 +34,10 @@ public class ManualMatchingController {
         return ApiResponse.response(HttpStatus.OK, CREATE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage(), roomId);
     }
 
+    @Operation(summary = "수동 매칭방 참여")
+    @PostMapping("/join")
+    public ApiResponse<Void> joinManualMatchingRoom(@Valid @RequestBody ManualMatchingJoinRequest request) {
+        manualMatchingService.joinManualMatchingRoom(request.userId(), request.roomId());
+        return ApiResponse.response(HttpStatus.OK, JOIN_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
+    }
 }
