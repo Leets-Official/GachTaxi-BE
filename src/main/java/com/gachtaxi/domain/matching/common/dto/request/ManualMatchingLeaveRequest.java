d7523c170a7b0b3353593e57fb56f5fb9e22aecf
huncozyboy <leejh5838@naver.com>
2025-01-31 23:01:05 +0900

    refactor: 불필요한 Dto 삭제

--- src/main/java/com/gachtaxi/domain/matching/common/dto/request/ManualMatchingLeaveRequest.java
+++
@@ -1,9 +0,0 @@
-package com.gachtaxi.domain.matching.common.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ManualMatchingLeaveRequest(
-        @NotNull Long userId,
-        @NotNull Long roomId
-) {
-}
