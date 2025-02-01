dacbd6be284722186db7d4c4ed3a03440170d187
huncozyboy <leejh5838@naver.com>
2025-02-02 01:06:34 +0900

    feat: 수동, 나의 매칭방 리스트 조회 컨트롤러 구현

--- src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
@@ -1,19 +1,24 @@
 package com.gachtaxi.domain.matching.common.controller;
 
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.CREATE_MANUAL_MATCHING_ROOM_SUCCESS;
+import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.GET_MANUAL_MATCHING_LIST_SUCCESS;
+import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.GET_MY_MATCHING_LIST_SUCCESS;
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.JOIN_MANUAL_MATCHING_ROOM_SUCCESS;
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.LEAVE_MANUAL_MATCHING_ROOM_SUCCESS;
 
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingJoinRequest;
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingRequest;
+import com.gachtaxi.domain.matching.common.dto.response.MatchingRoomResponse;
 import com.gachtaxi.domain.matching.common.service.ManualMatchingService;
 import com.gachtaxi.global.auth.jwt.annotation.CurrentMemberId;
 import com.gachtaxi.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +53,19 @@ public class ManualMatchingController {
     public ApiResponse<Void> leaveManualMatchingRoom(@CurrentMemberId Long userId, @PathVariable Long roomId) {
         manualMatchingService.leaveManualMatchingRoom(userId, roomId);
         return ApiResponse.response(HttpStatus.OK, LEAVE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
+    }
+
+    @Operation(summary = "수동 매칭방 조회")
+    @GetMapping("/list")
+    public ApiResponse<List<MatchingRoomResponse>> getManualMatchingList() {
+        List<MatchingRoomResponse> rooms = manualMatchingService.getManualMatchingList();
+        return ApiResponse.response(HttpStatus.OK, GET_MANUAL_MATCHING_LIST_SUCCESS.getMessage(), rooms);
+    }
+
+    @Operation(summary = "나의 매칭(수동) 조회")
+    @GetMapping("/my-list")
+    public ApiResponse<List<MatchingRoomResponse>> getMyMatchingList(@CurrentMemberId Long userId) {
+        List<MatchingRoomResponse> rooms = manualMatchingService.getMyMatchingList(userId);
+        return ApiResponse.response(HttpStatus.OK, GET_MY_MATCHING_LIST_SUCCESS.getMessage(), rooms);
     }
 }
