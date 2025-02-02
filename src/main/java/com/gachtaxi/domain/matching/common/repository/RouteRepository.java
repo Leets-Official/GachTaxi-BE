e7db0338cefcb49f4714cc29719c08b5d3b79b89
huncozyboy <leejh5838@naver.com>
2025-02-02 17:05:28 +0900

    feat : findByStartLocationNameAndEndLocationName 메서드 레포에서 구현

--- src/main/java/com/gachtaxi/domain/matching/common/repository/RouteRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/RouteRepository.java
@@ -1,10 +1,11 @@
 package com.gachtaxi.domain.matching.common.repository;
 
 import com.gachtaxi.domain.matching.common.entity.Route;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
-
+    Optional<Route> findByStartLocationNameAndEndLocationName(String startLocation, String endLocation);
 }
