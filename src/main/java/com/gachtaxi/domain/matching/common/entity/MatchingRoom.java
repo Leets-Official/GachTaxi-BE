197d66bc0bb88affa473814a8a0936d428a234ed
huncozyboy <leejh5838@naver.com>
2025-02-02 18:06:34 +0900

    refactor : 경로에 관한 정보는 이전과 동일하게 route 객체에 저장

--- src/main/java/com/gachtaxi/domain/matching/common/entity/MatchingRoom.java
+++ src/main/java/com/gachtaxi/domain/matching/common/entity/MatchingRoom.java
@@ -116,12 +116,13 @@ public class MatchingRoom extends BaseEntity {
         .build();
   }
 
-  public static MatchingRoom manualOf(Members roomMaster, String title, String description, int maxCapacity, int totalCharge, LocalDateTime departureTime) {
+  public static MatchingRoom manualOf(Members roomMaster, Route route, String title, String description, int maxCapacity, int totalCharge, LocalDateTime departureTime) {
     return MatchingRoom.builder()
             .capacity(4)
             .roomMaster(roomMaster)
             .title(title)
             .description(description)
+            .route(route)
             .totalCharge(totalCharge)
             .departureTime(departureTime)
             .matchingRoomType(MatchingRoomType.MANUAL)
