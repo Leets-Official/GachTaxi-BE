c2e1695f1146d67acc2a11f30c8d4a37114465b7
huncozyboy <leejh5838@naver.com>
2025-02-02 18:29:13 +0900

    refactor : 사용하지않는 메서드 삭제

--- src/main/java/com/gachtaxi/domain/matching/common/repository/RouteRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/RouteRepository.java
@@ -1,11 +1,9 @@
 package com.gachtaxi.domain.matching.common.repository;
 
 import com.gachtaxi.domain.matching.common.entity.Route;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
-    List<Route> findByStartLocationNameAndEndLocationName(String departure, String destination);
 }
