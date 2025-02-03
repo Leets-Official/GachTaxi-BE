27764917ea334a92ef57e6bc73c343d2163a88bc
huncozyboy <leejh5838@naver.com>
2025-02-03 20:23:39 +0900

    refactor : 정적 import를 활용해서 성공 응답코드 형태 통일

--- src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
@@ -6,6 +6,7 @@ import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.GET
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.GET_MY_MATCHING_LIST_SUCCESS;
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.JOIN_MANUAL_MATCHING_ROOM_SUCCESS;
 import static com.gachtaxi.domain.matching.common.controller.ResponseMessage.LEAVE_MANUAL_MATCHING_ROOM_SUCCESS;
+import static org.springframework.http.HttpStatus.OK;
 
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingJoinRequest;
 import com.gachtaxi.domain.matching.common.dto.request.ManualMatchingRequest;
@@ -20,7 +21,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,14 +43,14 @@ public class ManualMatchingController {
     @PostMapping("/creation")
     public ApiResponse<Long> createManualMatchingRoom(@CurrentMemberId Long userId, @Valid @RequestBody ManualMatchingRequest request) {
         Long roomId = manualMatchingService.createManualMatchingRoom(userId, request);
-        return ApiResponse.response(HttpStatus.OK, CREATE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage(), roomId);
+        return ApiResponse.response(OK, CREATE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage(), roomId);
     }
 
     @Operation(summary = "수동 매칭방 참여")
     @PostMapping("/join")
     public ApiResponse<Void> joinManualMatchingRoom(@CurrentMemberId Long userId, @Valid @RequestBody ManualMatchingJoinRequest request) {
         manualMatchingService.joinManualMatchingRoom(userId, request.roomId());
-        return ApiResponse.response(HttpStatus.OK, JOIN_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
+        return ApiResponse.response(OK, JOIN_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
     }
 
     @Operation(summary = "수동 매칭 초대 수락")
@@ -58,27 +58,27 @@ public class ManualMatchingController {
     public ApiResponse<Void> acceptInvitation(@RequestParam Long userId, @RequestParam Long matchingRoomId
     ) {
         matchingInvitationService.acceptInvitation(userId, matchingRoomId);
-        return ApiResponse.response(HttpStatus.OK, ACCEPT_MATCHING_INVITE_SUCCESS.getMessage());
+        return ApiResponse.response(OK, ACCEPT_MATCHING_INVITE_SUCCESS.getMessage());
     }
 
     @Operation(summary = "수동 매칭방 퇴장 (방 삭제 포함)")
     @PatchMapping("/exit/{roomId}")
     public ApiResponse<Void> leaveManualMatchingRoom(@CurrentMemberId Long userId, @PathVariable Long roomId) {
         manualMatchingService.leaveManualMatchingRoom(userId, roomId);
-        return ApiResponse.response(HttpStatus.OK, LEAVE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
+        return ApiResponse.response(OK, LEAVE_MANUAL_MATCHING_ROOM_SUCCESS.getMessage());
     }
 
     @Operation(summary = "수동 매칭방 조회")
     @GetMapping("/list")
     public ApiResponse<MatchingRoomListResponse> getManualMatchingList(int pageNumber, int pageSize) {
         Page<MatchingRoomResponse> rooms = manualMatchingService.getManualMatchingList(pageNumber, pageSize);
-        return ApiResponse.response(HttpStatus.OK, GET_MANUAL_MATCHING_LIST_SUCCESS.getMessage(), MatchingRoomListResponse.of(rooms));
+        return ApiResponse.response(OK, GET_MANUAL_MATCHING_LIST_SUCCESS.getMessage(), MatchingRoomListResponse.of(rooms));
     }
 
     @Operation(summary = "나의 매칭(수동) 조회")
     @GetMapping("/my-list")
     public ApiResponse<MatchingRoomListResponse> getMyMatchingList(@CurrentMemberId Long userId, int pageNumber, int pageSize) {
         Page<MatchingRoomResponse> rooms = manualMatchingService.getMyMatchingList(userId, pageNumber, pageSize);
-        return ApiResponse.response(HttpStatus.OK, GET_MY_MATCHING_LIST_SUCCESS.getMessage(), MatchingRoomListResponse.of(rooms));
+        return ApiResponse.response(OK, GET_MY_MATCHING_LIST_SUCCESS.getMessage(), MatchingRoomListResponse.of(rooms));
     }
 }
