1978175a028d1c5a4c7f3f418b0f668f8c3f7372
huncozyboy <leejh5838@naver.com>
2025-02-03 21:32:49 +0900

    refactor : 사용하지않는 메서드 삭제

--- src/main/java/com/gachtaxi/domain/matching/common/entity/MatchingRoom.java
+++ src/main/java/com/gachtaxi/domain/matching/common/entity/MatchingRoom.java
@@ -3,7 +3,6 @@ package com.gachtaxi.domain.matching.common.entity;
 import com.gachtaxi.domain.matching.algorithm.dto.FindRoomResult;
 import com.gachtaxi.domain.matching.common.entity.enums.MatchingRoomStatus;
 import com.gachtaxi.domain.matching.common.entity.enums.MatchingRoomType;
-import com.gachtaxi.domain.matching.common.exception.MatchingRoomAlreadyFullException;
 import com.gachtaxi.domain.matching.event.dto.kafka_topic.MatchRoomCreatedEvent;
 import com.gachtaxi.domain.matching.common.entity.enums.Tags;
 import com.gachtaxi.domain.members.entity.Members;
@@ -49,7 +48,7 @@ public class MatchingRoom extends BaseEntity {
   @Setter
   private Members roomMaster;
 
-  @Column(name = "title", nullable = false)
+  @Column(name = "title")
   @Getter
   private String title;
 
@@ -153,13 +152,5 @@ public class MatchingRoom extends BaseEntity {
     return this.matchingRoomTagInfo.stream()
             .map(tagInfo -> tagInfo.getTags().name())
             .toList();
-  }
-
-  public void addMember(Members member) {
-    if (this.getCurrentMemberCount() >= this.capacity) {
-      throw new MatchingRoomAlreadyFullException();
-    }
-    MemberMatchingRoomChargingInfo memberInfo = MemberMatchingRoomChargingInfo.notPayedOf(this, member);
-    this.memberMatchingRoomChargingInfo.add(memberInfo);
   }
 }
