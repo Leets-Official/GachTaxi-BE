52dc5b46b21b2f058c6f1cc2a7c32bbbca93d60d
koreaioi <koreaioi@gachon.ac.kr>
2025-02-03 15:32:19 +0900

    fix: FriendsUpdateDto 수정

--- src/main/java/com/gachtaxi/domain/friend/dto/request/FriendUpdateDto.java
+++ src/main/java/com/gachtaxi/domain/friend/dto/request/FriendUpdateDto.java
@@ -3,6 +3,7 @@ package com.gachtaxi.domain.friend.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record FriendUpdateDto(
-        @NotNull Long memberId
+        @NotNull Long memberId,
+        @NotNull String notificationId
 ) {
 }
