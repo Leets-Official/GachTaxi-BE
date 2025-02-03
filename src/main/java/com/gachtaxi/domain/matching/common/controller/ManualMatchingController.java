7b9ec1e4fbc6bf0d9dfdab335c1a99c2cc1ada36
huncozyboy <leejh5838@naver.com>
2025-02-04 02:09:03 +0900

    refactor : 모든 테스트 완료 후 CurrentMemberId 어노테이션 사용하도록 재변경

--- src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/ManualMatchingController.java
@@ -55,7 +55,7 @@ public class ManualMatchingController {
 
     @Operation(summary = "수동 매칭 초대 수락")
     @PostMapping("/invite/accept")
-    public ApiResponse<Void> acceptInvitation(@RequestParam Long userId, @RequestParam Long matchingRoomId, @RequestParam String notificationId
+    public ApiResponse<Void> acceptInvitation(@CurrentMemberId Long userId, @RequestParam Long matchingRoomId, @RequestParam String notificationId
     ) {
         matchingInvitationService.acceptInvitation(userId, matchingRoomId, notificationId);
         return ApiResponse.response(OK, ACCEPT_MATCHING_INVITE_SUCCESS.getMessage());
