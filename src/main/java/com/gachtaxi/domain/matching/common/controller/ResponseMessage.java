e5f3e1405f61ab51b22cbc9048b5f9cde44d4d7a
huncozyboy <leejh5838@naver.com>
2025-02-05 00:24:57 +0900

    refactor : 매칭방 초대/수락 Api 응답 메세지 통일

--- src/main/java/com/gachtaxi/domain/matching/common/controller/ResponseMessage.java
+++ src/main/java/com/gachtaxi/domain/matching/common/controller/ResponseMessage.java
@@ -22,7 +22,7 @@ public enum ResponseMessage {
   CONVERT_TO_AUTO_MATCHING_SUCCESS("자동 매칭으로 전환되었습니다."),
   GET_MANUAL_MATCHING_LIST_SUCCESS("수동 매칭방 조회에 성공했습니다."),
   GET_MY_MATCHING_LIST_SUCCESS("내 매칭방 조회에 성공했습니다."),
-  ACCEPT_MATCHING_INVITE_SUCCESS("매칭방 초대 수락에 성공했습니다");
+  ACCEPT_MATCHING_INVITE_SUCCESS("매칭방 초대를 수락/거절이 완료되었습니다");
 
   private final String message;
 }
