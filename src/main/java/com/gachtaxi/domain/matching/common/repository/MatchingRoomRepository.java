0b0d30ce0b885c8a31ce95137163d51f4e843731
huncozyboy <leejh5838@naver.com>
2025-02-05 13:41:45 +0900

    refactor : 나의 매칭도 ACTIVE 상태인 방만 필터링

--- src/main/java/com/gachtaxi/domain/matching/common/repository/MatchingRoomRepository.java
+++ src/main/java/com/gachtaxi/domain/matching/common/repository/MatchingRoomRepository.java
@@ -5,7 +5,6 @@ import com.gachtaxi.domain.matching.common.entity.enums.MatchingRoomStatus;
 import com.gachtaxi.domain.matching.common.entity.enums.MatchingRoomType;
 import com.gachtaxi.domain.members.entity.Members;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -42,6 +41,9 @@ public interface MatchingRoomRepository extends JpaRepository<MatchingRoom, Long
 
     Page<MatchingRoom> findByMatchingRoomTypeAndMatchingRoomStatus(MatchingRoomType type, MatchingRoomStatus status, Pageable pageable);
 
-    @Query("SELECT m.matchingRoom FROM MemberMatchingRoomChargingInfo m WHERE m.members = :user ORDER BY m.matchingRoom.id DESC")
+    @Query("SELECT m.matchingRoom FROM MemberMatchingRoomChargingInfo m " +
+            "WHERE m.members = :user " +
+            "AND m.matchingRoom.matchingRoomStatus = 'ACTIVE' " +
+            "ORDER BY m.matchingRoom.id DESC")
     Page<MatchingRoom> findByMemberInMatchingRoom(@Param("user") Members user, Pageable pageable);
 }
