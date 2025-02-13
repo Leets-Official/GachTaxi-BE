0f622e397c8d17cfb9eac9f6a420c526d1b8d866
hyxklee <ewgt1234@naver.com>
2025-02-13 10:21:45 +0900

    refactor: 상수 public으로 변경

--- src/main/java/com/gachtaxi/domain/chat/redis/RedisChatSubscriber.java
+++ src/main/java/com/gachtaxi/domain/chat/redis/RedisChatSubscriber.java
@@ -5,7 +5,6 @@ import com.gachtaxi.domain.chat.exception.CustomMessagingException;
 import com.gachtaxi.domain.chat.exception.CustomSerializationException;
 import com.gachtaxi.domain.chat.exception.RedisSubscribeException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,12 +13,11 @@ import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RedisChatSubscriber implements MessageListener {
 
-    private static final String CHAT_ROOM_PREFIX = "/sub/chat/room/";
+    public static final String CHAT_ROOM_PREFIX = "/sub/chat/room/";
 
     private final RedisTemplate<String, ChatMessage> chatRedisTemplate;
     private final SimpMessageSendingOperations simpMessageSendingOperations;
