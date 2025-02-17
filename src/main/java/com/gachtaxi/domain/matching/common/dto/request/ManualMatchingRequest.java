8931818aac7b3fe14ae612545a87a6a8852c07e7
huncozyboy <leejh5838@naver.com>
2025-02-17 22:48:14 +0900

    refactor : ISO 형식으로 변수형 통일 및 불필요한 get 메서드 삭제

--- src/main/java/com/gachtaxi/domain/matching/common/dto/request/ManualMatchingRequest.java
+++ src/main/java/com/gachtaxi/domain/matching/common/dto/request/ManualMatchingRequest.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record ManualMatchingRequest(
@@ -12,20 +14,20 @@ public record ManualMatchingRequest(
         String description,
 
         @NotBlank
-        String startName,
+        String departure,
 
         @NotBlank
-        String destinationName,
+        String destination,
 
         @NotNull
-        String departureTime,
+        LocalDateTime departureTime,
 
         @NotNull
-        String departureDate,
+        LocalDate departureDate,
 
         @Schema(description = "예상 요금")
         @Min(value = 4000)
-        int expectedTotalCharge,
+        int totalCharge,
 
         @Schema(description = "매칭 태그")
         List<String> criteria,
@@ -41,17 +43,5 @@ public record ManualMatchingRequest(
 
     public List<Long> getFriendsId() {
         return members;
-    }
-
-    public int getTotalCharge() {
-        return expectedTotalCharge;
-    }
-
-    public String getDeparture() {
-        return startName;
-    }
-
-    public String getDestination() {
-        return destinationName;
     }
 }
