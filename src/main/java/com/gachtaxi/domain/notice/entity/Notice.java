6e2e6a54e4a8869c7cf477da9ca434ad14c1be50
huncozyboy <leejh5838@naver.com>
2025-04-13 02:18:40 +0900

    feat : 공지사항 엔티티 구현

---
+++ src/main/java/com/gachtaxi/domain/notice/entity/Notice.java
@@ -0,0 +1,30 @@
+package com.gachtaxi.domain.notice.entity;
+
+
+import com.gachtaxi.global.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SuperBuilder
+public class Notice extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+}
