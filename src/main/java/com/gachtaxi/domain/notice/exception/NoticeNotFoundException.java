9a3f2ca102cfd93853b84a3a54be1334295b370c
huncozyboy <leejh5838@naver.com>
2025-04-13 02:22:15 +0900

    fix : 올바른 예외처리 메세지 사용하도록 수정

--- src/main/java/com/gachtaxi/domain/notice/exception/NoticeNotFoundException.java
+++ src/main/java/com/gachtaxi/domain/notice/exception/NoticeNotFoundException.java
@@ -1,12 +1,12 @@
 package com.gachtaxi.domain.notice.exception;
 
-import static com.gachtaxi.domain.chat.exception.ErrorMessage.CHATTING_ROOM_NOT_FOUND;
+import static com.gachtaxi.domain.notice.exception.ErrorMessage.NOTICE_NOT_FOUND;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import com.gachtaxi.global.common.exception.BaseException;
 
 public class NoticeNotFoundException extends BaseException {
 
-    public NoticeNotFoundException() { super(NOT_FOUND, CHATTING_ROOM_NOT_FOUND.getMessage()); }
+    public NoticeNotFoundException() { super(NOT_FOUND, NOTICE_NOT_FOUND.getMessage()); }
 
 }
