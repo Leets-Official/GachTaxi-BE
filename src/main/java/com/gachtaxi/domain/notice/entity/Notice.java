c4053048dfe100286add6e94b227204ab356ea75
huncozyboy <leejh5838@naver.com>
2025-04-15 21:51:45 +0900

    refactor : 중복된 엔티티 필드 삭제

--- src/main/java/com/gachtaxi/domain/notice/entity/Notice.java
+++ src/main/java/com/gachtaxi/domain/notice/entity/Notice.java
@@ -4,9 +4,6 @@ package com.gachtaxi.domain.notice.entity;
 import com.gachtaxi.global.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +14,6 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder
 public class Notice extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     private String title;
 
